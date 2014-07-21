954ffc8a035a96eb6830fa56543d7210ce473fd1
Jacques Carette <carette@mcmaster.ca>
2014-07-21 13:34:21 -0400

    continue to make the pretty printer more semantic

--- Language/Hakaru/Symbolic.hs
+++ Language/Hakaru/Symbolic.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE GADTs, TypeFamilies #-}
+{-# LANGUAGE GADTs, TypeFamilies, ScopedTypeVariables #-}
 {-# OPTIONS -Wall #-}
 
 module Language.Hakaru.Symbolic where
@@ -28,8 +28,9 @@ class Conditioning repr where
   conditioned, unconditioned :: repr (Dist a) -> repr (Measure a)
 
 -- Printer (to Maple)
+data Pos = Front | Back
 type VarCounter = Int
-newtype Maple a = Maple { unMaple :: Bool -> VarCounter -> String }
+newtype Maple a = Maple { unMaple :: Pos -> VarCounter -> String }
 
 -- if it weren't for the constraints, we could/should use Applicative
 pure :: Show a => a -> Maple a
@@ -54,6 +55,9 @@ d2m e = Maple $ unMaple e
 infixPr :: String -> (String -> String -> String)
 infixPr s a b = a ++ s ++ b
 
+reify :: forall a. Read a => Pos -> VarCounter -> Maple a -> a
+reify f h e = (read (unMaple e f h) :: a)
+
 instance RealComp Maple where
   real  = pure
   add   = liftA2 $ infixPr "+"
@@ -69,33 +73,36 @@ instance BoolComp Maple where
 
 instance MeasMonad Maple where
   ret      = liftA1M $ mkPr "g"
-  bind m c = Maple $ \f h -> unMaple m True h ++ 
+  bind m c = Maple $ \f h -> unMaple m Front h ++ 
                     unMaple (c (Maple $ \_ _ -> ("x" ++ show h))) (f) (succ h)
-                    ++ unMaple m False h 
+                    ++ unMaple m Back h 
 
 instance Distrib Maple where
-  uniform e1 e2  = Maple $ \f h -> if f == True then  
-                  show (1/((read (unMaple e2 f h) :: Double) - 
-                  (read (unMaple e1 f h) :: Double))) ++ " * Int (" 
-                  else ", x" ++ show h ++ "=" ++ unMaple e1 f h ++ ".." ++ 
-                  unMaple e2 f h ++ ")"
-  uniformD e1 e2 = Maple $ \f h -> if f == True then  
-                  show (1/((read (unMaple e2 f h) :: Double) - 
-                  (read (unMaple e1 f h) :: Double))) ++ " * Sum (" 
-                  else ", x" ++ show h ++ "=" ++ unMaple e1 f h ++ ".." ++ 
-                  unMaple e2 f h ++ ")"                  
-  normal e1 e2     = Maple $ \f h -> if f == True then  
-                  "Int (PDF (Normal (" ++ unMaple e1 f h ++ ", " ++
-                  unMaple e2 f h ++ ", x" ++ show h ++ ") * "  
-                  else ", x" ++ show h ++ "=" ++ unMaple e1 f h ++ ".." ++ 
-                  unMaple e2 f h ++ ")"              
+  uniform e1 e2  = Maple pr
+    where
+      pr Front h = let rd a = reify Front h a :: Double in
+                   show (1/((rd e2) - (rd e1))) ++ " * Int (" 
+      pr Back h  = ", x" ++ show h ++ "=" ++ unMaple e1 Back h ++ 
+                    ".." ++ unMaple e2 Back h ++ ")"
+  uniformD e1 e2 = Maple pr
+    where
+      pr Front h = let rd a = reify Front h a :: Double in
+                   show (1/((rd e2) - (rd e1))) ++ " * Sum (" 
+      pr Back h  = ", x" ++ show h ++ "=" ++ unMaple e1 Back h ++ 
+                    ".." ++ unMaple e2 Back h ++ ")"
+  normal e1 e2 = Maple pr
+    where
+      pr Front h = "Int (PDF (Normal (" ++ unMaple e1 Front h ++ ", " ++
+                   unMaple e2 Front h ++ ", x" ++ show h ++ ") * "  
+      pr Back  h = ", x" ++ show h ++ "=" ++ unMaple e1 Back h ++ ".." ++ 
+                   unMaple e2 Back h ++ ")"              
 
 instance Conditioning Maple where
   unconditioned = d2m
   conditioned   = d2m
 
 view :: Maple a -> String
-view e = unMaple e True 0
+view e = unMaple e Front 0
 
 -- TEST CASES
 exp1, exp2, exp3 :: Maple (Measure Double)
