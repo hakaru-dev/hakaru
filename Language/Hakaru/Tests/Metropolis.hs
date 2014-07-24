71c1edefd5af7acf4ab8235b9668ff538fe7151b
Rob Zinkov <rob@zinkov.com>
2014-07-23 23:36:08 -0400

    Made metropolis tests work

--- Language/Hakaru/Tests/Metropolis.hs
+++ Language/Hakaru/Tests/Metropolis.hs
@@ -4,6 +4,7 @@ module Language.Hakaru.Tests.Metropolis where
 
 import Data.Dynamic
 
+import Language.Hakaru.Types
 import Language.Hakaru.Lambda
 import Language.Hakaru.Metropolis
 import Language.Hakaru.Distribution (bern, normal, uniform, beta)
@@ -14,12 +15,12 @@ import Distribution.TestSuite.QuickCheck
 prog_mixture :: Measure Bool
 prog_mixture = do
   c <- unconditioned (bern 0.5)
-  _ <- conditioned (ifThenElse c (normal (lit (1 :: Double)) (lit 1))
-                                 (uniform (lit 0) (lit 3)))
+  _ <- conditioned (ifThenElse c (normal  1 1)
+                                 (uniform 0 3))
   return c
 
 test_mixture :: IO [Bool]
-test_mixture = mcmc prog_mixture [Just (toDyn (-2 :: Double))]
+test_mixture = mcmc prog_mixture [Just (toDyn (Lebesgue (-2) :: Density Double))]
 
 prog_multiple_conditions :: Measure Double
 prog_multiple_conditions = do
@@ -28,10 +29,10 @@ prog_multiple_conditions = do
   _ <- conditioned (bern b)
   return b
 
-test_multiple_conditions :: IO ()
+test_multiple_conditions :: IO [Double]
 test_multiple_conditions = do
-  l <- mcmc prog_multiple_conditions [Just (toDyn True), Just (toDyn False)]
-  viz 10000 ["beta"] (map return l)
+  mcmc prog_multiple_conditions [Just (toDyn (Discrete True)),
+                                 Just (toDyn (Discrete False))]
 
 prog_two_normals :: Measure Bool
 prog_two_normals = unconditioned (bern 0.5) `bind` \coin ->
@@ -40,9 +41,7 @@ prog_two_normals = unconditioned (bern 0.5) `bind` \coin ->
        return coin
 
 test_two_normals :: IO [Bool]
-test_two_normals = mcmc prog_two_normals [Just (toDyn (1 :: Double))]
+test_two_normals = mcmc prog_two_normals [Just (toDyn (Lebesgue 1 :: Density Double))]
 
-test_normal :: IO ()
-test_normal = do
-  l <- mcmc (unconditioned (normal 1 3)) [] :: IO [Double]
-  viz 10000 ["normal"] (map return l)
+test_normal :: IO [Double]
+test_normal = mcmc (unconditioned (normal 1 3)) []
