feb54ef1d1643192e65cf62ff4dee6e42ac9858f
Praveen Narayanan <revenap@gmail.com>
2014-08-20 17:47:28 -0400

    Committing intermediate mixture model code for merge

--- Examples/Examples.hs
+++ Examples/Examples.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE RankNTypes, DataKinds, NoMonomorphismRestriction, BangPatterns #-}
 
-module Examples (test_gaussian_mixture) where
+module Examples where
 
 import Language.Hakaru.Types
 import Data.Dynamic
@@ -9,6 +9,7 @@ import Control.Monad
 import Language.Hakaru.ImportanceSampler
 import Language.Hakaru.Distribution
 import Language.Hakaru.Lambda
+import Language.Hakaru.Util.Visual
 
 bayesian_polynomial_regression = undefined
 
@@ -26,19 +27,24 @@ latent_dirichelt_allocation = undefined
 
 categorical_mixture = undefined
 
-gaussian_mixture :: Measure [Double]
-gaussian_mixture = do
+nPoints :: Int
+nPoints = 6
+
+gaussianMixture :: Measure [Double]
+gaussianMixture = do
   p <- unconditioned (beta 2 2)
-  m1:m2:_ <- replicateM 2 $ unconditioned (normal 0 10)
-  s1:s2:_ <- replicateM 2 $ unconditioned (uniform 0 1)
+  m1:m2:_ <- replicateM 2 $ unconditioned (normal 100 30)
+  s1:s2:_ <- replicateM 2 $ unconditioned (uniform 0 2)
   let makePoint = do        
         b <- unconditioned (bern p)
         unconditioned (ifThenElse b (normal m1 s1)
                                     (normal m2 s2))
-  replicateM 20 makePoint
+  replicateM nPoints makePoint
 
-test_gaussian_mixture :: IO ()
-test_gaussian_mixture = sample gaussian_mixture [] >>= print . take 1
+testGaussianMixture :: IO ()
+testGaussianMixture = 
+    sample gaussianMixture [] >>= viz nPoints ["x"] . convert . take 1
+    where convert [(xs,_)] = [[x] | x <- xs]
 
 naive_bayes = undefined
 
