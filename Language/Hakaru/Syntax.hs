142c8664b5a7b7429562d121e7d85d616da2afbe
Rob Zinkov <rob@zinkov.com>
2014-11-21 12:57:57 -0500

    Added default poisson definition

--- Language/Hakaru/Syntax.hs
+++ Language/Hakaru/Syntax.hs
@@ -145,6 +145,11 @@ min_, max_ :: (Order repr a, Base repr) => repr a -> repr a -> repr a
 min_ x y = if_ (less x y) x y
 max_ x y = if_ (less x y) y x
 
+fact :: Base repr => repr Int -> repr Int
+fact n = if_ (less 0 n)
+         1
+         (fact (n -1))
+
 class (Base repr) => Mochastic repr where
   dirac         :: repr a -> repr (Measure a)
   bind          :: repr (Measure a) ->
@@ -175,6 +180,12 @@ class (Base repr) => Mochastic repr where
 
   poisson       :: repr Prob -> repr (Measure Int)
   -- TODO: default implementation of poisson in terms of countInt
+  poisson l     =  countInt `bind` \x ->
+                   if_ (and_ [less 0 x, less 0 l])
+                       (superpose [((pow_ l (fromInt x)) /
+                                    (unsafeProb (fromInt (fact x))) *
+                                    (exp_ (- (fromInt x))), dirac x)])
+                       (superpose [])
 
   gamma :: repr Prob -> repr Prob -> repr (Measure Prob)
   gamma shape scale =
