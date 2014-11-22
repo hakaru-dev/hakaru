413ffa16cd8d37dc78105334033756420eb3c087
Chung-chieh Shan <ccshan@indiana.edu>
2014-11-21 20:56:25 -0500

    Export more Hakaru symbols to runInterpreter

--- Language/Hakaru/RoundTrip.hs
+++ Language/Hakaru/RoundTrip.hs
@@ -1,4 +1,4 @@
-module Language.Hakaru.RoundTrip (Num(..), Fractional(..), Floating(..), ($), Any(..), module Language.Hakaru.Syntax) where
+module Language.Hakaru.RoundTrip (()(..), Either(..), (,)(..), Num(..), Fractional(..), Floating(..), ($), Any(..), module Language.Hakaru.Syntax) where
 
 import Language.Hakaru.Syntax
 import Language.Hakaru.Maple (Any(..))
