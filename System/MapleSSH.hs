82d4a4da90188752fe04a5c89fea2e2b3eac5f62
Mike Kucera <mikekucera@gmail.com>
2014-11-22 14:22:24 -0500

    Simplify MapleSSH

--- System/MapleSSH.hs
+++ System/MapleSSH.hs
@@ -1,12 +1,14 @@
-module System.MapleSSH (maple, withMaple) where
+module System.MapleSSH (maple) where
 
 import Control.Monad
 import Control.Concurrent
 import Data.List
-import Data.Maybe
+import Data.Maybe(fromMaybe)
+import Data.Char(isSpace)
 import System.IO
 import System.Process
 import System.Environment
+import System.Exit
 
 -- Default values for environment variables
 defSSH    = "/usr/bin/ssh"
@@ -14,8 +16,6 @@ defUser   = "ppaml"
 defServer = "quarry.uits.indiana.edu"
 defModule = "maple/18"
 
-maple :: String -> IO String
-maple x = withMaple $ \eval -> eval x
 
 envVars :: IO (String, String, String, String)
 envVars = do ssh    <- get "MAPLE_SSH"    defSSH
@@ -26,43 +26,26 @@ envVars = do ssh    <- get "MAPLE_SSH"    defSSH
              where env = getEnvironment
                    get name def = fmap (fromMaybe def . lookup name) env
 
-mkMaple :: IO (Handle, Handle, ProcessHandle)
-mkMaple = do
-    (ssh, user, server, mapleModule) <- envVars
-    let commands = "module load -s " ++ mapleModule ++ "; maple -q" -- quiet mode
-    (Just inH, Just outH, Nothing, pid) <- createProcess (proc ssh ["-l" ++ user, server, commands]){ std_in = CreatePipe, std_out = CreatePipe, close_fds = True }
-    let configHandle h = hSetBuffering h NoBuffering
-    mapM_ configHandle [inH, outH]
-    return (inH, outH, pid)
 
-readUntil :: Handle -> String -> IO String
-readUntil hdl str = fmap reverse (readUntil' (reverse str) [])
-  where readUntil' xs ys | Just ys' <- stripPrefix xs ys
-                         = return ys'
-                         | otherwise
-                         = do c <- hGetChar hdl
-                              readUntil' xs (c:ys)
+maple :: String -> IO String
+maple x = do
+    (ssh, user, server, mod) <- envVars
+    let commands = "module load -s " ++ mod ++ "; maple -q" -- quiet mode
+    (Just inH, Just outH, Nothing, p) <- createProcess (proc ssh ["-l" ++ user, server, commands])
+                                         { std_in = CreatePipe, std_out = CreatePipe, close_fds = True }
+    hPutStrLn inH $ x ++ ";"
+    hClose inH
+    c <- hGetContents outH
+    length c `seq` hClose outH
+    exit <- waitForProcess p
+    case exit of ExitSuccess -> return $ trim c
+                 _ -> error ("maple:" ++ show exit)
 
-withMaple :: ((String -> IO String) -> IO a) -> IO a
-withMaple k =
-  do cmd <- newChan
-     rsp <- newChan
-     (inH, outH, _) <- mkMaple
-     let term = "---"
-     tid <- forkIO . forever $
-              do c <- readChan cmd
-                 let c' = c ++ ";printf(\"%s\",\"" ++ term ++ "\");" -- print a terminator
-                 hPutStrLn inH c'
-                 o <- fmap (unwords . words) $ readUntil outH term
-                 writeChan rsp o
-     r <- k $ \c -> do writeChan cmd c
-                       readChan rsp
-     killThread tid
-     return r
+
+trim :: String -> String
+trim = dropWhile isSpace
+
 
 test :: IO ()
-test = withMaple $ \eval ->
-        do x <- eval "1 + 1"
-           y <- eval "eval(SLO)"
-           print (x,y)
-
+test = do x <- maple "eval(SLO)"
+          print x
