733760cb2b5c23cfc12bb7b6149808399458e772
Chung-chieh Shan <ccshan@indiana.edu>
2014-12-07 15:17:23 -0500

    More practical one-bird inference?

--- Examples/Bird1.hs
+++ Examples/Bird1.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE Rank2Types, DataKinds #-}
 {-# OPTIONS -W #-}
 
-module Main where
+module Main (main) where
 
 -- Bird0 (one-bird model) in Hakaru 0.2
 
@@ -10,12 +10,13 @@ import Language.Hakaru.Util.Csv
 import Language.Hakaru.Syntax hiding (liftM)
 import Language.Hakaru.Vector hiding (mapM)
 import Language.Hakaru.Sample (unSample)
-import Language.Hakaru.Partial (runPartial, dynamic)
+import Language.Hakaru.Partial (Partial, runPartial, dynamic)
 import Language.Hakaru.PrettyPrint (runPrettyPrint)
 import System.Random.MWC (withSystemRandom)
 import System.IO (stderr, hPutStrLn)
 import System.Environment (getArgs, getProgName)
 import System.Exit (exitFailure)
+import Data.Maybe (isNothing)
 import Data.Array.Unboxed
 import Control.Monad (replicateM_)
 
@@ -64,26 +65,40 @@ readObservations observationsFile = do
            | year:::day:::counts <- rows
            , (cell,count) <- zip [1..] counts ]
 
-birdYear :: (Mochastic repr) =>
+partials :: (Mochastic repr) =>
+            Partial repr Cell ->
+            [(Partial repr Cell -> Partial repr (Measure Cell), Maybe Int)] ->
+            repr (Measure ())
+partials from l = case span (isNothing . snd) l of
+  (_, []) -> dirac unit
+  (unobserved, (observed, Just observation) : rest) ->
+    let bunch = runPartial
+              $ foldr (\(t,Nothing) c from -> t from `bind` c)
+                      (\from -> observed from `bind` \to ->
+                                if_ (to `equal` fromIntegral observation)
+                                    (dirac unit)
+                                    (superpose []))
+                      unobserved
+                      from
+        r = partials (fromIntegral observation) rest
+    in bunch `bind_` r
+
+birdYear :: (Mochastic repr, Lambda repr) =>
             (Day -> Cell -> Cell -> Feature -> Double) ->
             (Day -> Cell -> Int) ->
-            Repeat NFeature (repr Real) -> repr (Measure Cell)
-birdYear features obs = go nday where
-  go :: (Mochastic repr) =>
-        Day -> Repeat NFeature (repr Real) -> repr (Measure Cell)
-  go day@1 _      = observe 1 (obs day)
-  go day   params = let params' = dynamic <$> params in runPartial $
-                    go (day - 1) params' `bind` \from ->
-                    transition params' (day - 1) from `bind` \to ->
-                    observe to (obs day)
-  observe :: (Mochastic repr) =>
-             repr Cell -> (Cell -> Int) -> repr (Measure Cell)
-  observe cell o = case [ c | c <- [1..ncell], o c > 0 ] of
-                     []  -> dirac cell
-                     [c] -> if_ (cell `equal` fromIntegral c)
-                                (dirac cell)
-                                (superpose [])
-                     _   -> error "Birds observed in multiple cells at once!?"
+            Repeat NFeature (repr Real) -> repr (Measure ())
+birdYear features obs params =
+  partials 1
+           [ ( if day > 1 then transition params' (day - 1) else dirac
+             , observe (obs day) )
+           | day <- [1..nday] ]
+ where
+  params' = dynamic <$> params
+  observe :: (Cell -> Int) -> Maybe Int
+  observe o = case [ c | c <- [1..ncell], o c > 0 ] of
+                []  -> Nothing
+                [c] -> Just c
+                _   -> error "Birds observed in multiple cells at once!?"
   transition :: (Mochastic repr) => Repeat NFeature (repr Real) ->
                 Day -> repr Cell -> repr (Measure Cell)
   transition params day from = reflect from (\f -> categorical
@@ -99,29 +114,18 @@ birdYear features obs = go nday where
   fromDouble :: (Base repr) => Double -> repr Real
   fromDouble = fromRational . toRational
 
-birdYear' :: forall repr. (Mochastic repr) =>
-             (Day -> Cell -> Cell -> Feature -> Double) ->
-             (Day -> Cell -> Int) ->
-             Repeat NFeature (repr Real) -> repr (Measure ())
-birdYear' features obs params =
-  runPartial (birdYear features obs (dynamic <$> params) `bind_` dirac unit)
-
-bird :: (Mochastic repr) =>
+bird :: (Mochastic repr, Lambda repr) =>
         UArray (Year, Day, Cell, Cell, Feature) Double ->
         UArray (Year, Day, Cell) Int ->
         repr (Measure (Repeat NFeature Real))
 bird features observations =
-  normal 0 10 `bind` \p1 ->
-  normal 0 10 `bind` \p2 ->
-  normal 0 10 `bind` \p3 ->
-  normal 0 10 `bind` \p4 ->
-  let params = (p1, (p2, (p3, (p4, ()))))
-      g k year = let f day from to i =
+  mapC (\() c -> normal 0 10 `bind` c) (pure ()) $ \params ->
+  let g k year = let f day from to i =
                        features ! (year, day, from, to, i)
                      o day cell =
                        observations ! (year, day, cell)
                  in birdYear f o params `bind_` k
-  in foldl g (dirac (toNestedPair params)) [1{-..nyear-}]
+  in foldl g (dirac (toNestedPair params)) [1..nyear]
 
 main :: IO ()
 main = do
@@ -134,9 +138,10 @@ main = do
       observations <- readObservations observationsFile
       hPutStrLn stderr "Building model"
       let m = bird features observations
-      --writeFile "/tmp/m" (show (runPrettyPrint m))
+      -- writeFile "/tmp/m" (show (runPrettyPrint m))
       let s = unSample m 1
-      withSystemRandom (\g -> replicateM_ 20 (s g >>= print))
+      hPutStrLn stderr "Sampling"
+      withSystemRandom (\g -> replicateM_ 2000 (s g >>= maybe (return ()) print))
     _ -> do
       progName <- getProgName
       hPutStrLn stderr ("Usage: " ++ progName ++ " \
