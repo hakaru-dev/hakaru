33ba566fabe3ef4caa901f4ebbf6b9f514c07386
Natalie Perna <nmperna@gmail.com>
2015-01-28 00:39:34 -0500

    Test distribution relationship transformations from Gamma to Chi-Squared

--- Tests/Relationships.hs
+++ Tests/Relationships.hs
@@ -10,7 +10,9 @@ import Tests.TestTools
 
 testRelationships :: Test
 testRelationships = test [
-    "t1"   ~: testSS [t1] (lam (\mu -> (lam (\sigma -> normal 0 1))))
+    "t1"   ~: testSS [t1] (lam (\mu -> (lam (\sigma -> normal 0 1)))),
+    "t2"   ~: testSS [t2] (lam (\beta -> chi2 (2*beta))),
+    "t3"   ~: testSS [t3] (lam (\alpha -> (lam (\beta -> chi2 (2*beta)))))
     ]
 
 allTests :: Test
@@ -20,3 +22,9 @@ allTests = test [
 
 t1 :: (Lambda repr, Mochastic repr) => repr (Real -> Prob -> Measure Real)
 t1 = lam (\mu -> (lam (\sigma -> normal mu sigma `bind` \x -> dirac ((x - mu) / (fromProb sigma)))))
+
+t2 :: (Lambda repr, Mochastic repr) => repr (Prob -> Measure Prob)
+t2 = (lam (\beta -> gamma 2 beta))
+
+t3 :: (Lambda repr, Mochastic repr) => repr (Prob -> Prob -> Measure Prob)
+t3 = lam (\alpha -> (lam (\beta -> gamma alpha beta `bind` \x -> dirac (2 * x / alpha))))
