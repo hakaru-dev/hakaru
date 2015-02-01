a8f00c7f49341cd7863b2ccbd1e07537274594c4
Rob Zinkov <rob@zinkov.com>
2015-02-01 14:17:19 -0500

    Redefine dirichlet in terms of un-normalized dirichlet

--- Language/Hakaru/Syntax.hs
+++ Language/Hakaru/Syntax.hs
@@ -341,11 +341,15 @@ sumVec x = summate (fromInt $ loBound x)
                    (fromInt $ hiBound x)
                    (\ i -> index x i)
 
+unNormedDirichlet :: Mochastic repr =>
+                     repr (Vector Prob) -> repr (Measure (Vector Prob))
+unNormedDirichlet a = plate $ vector (loBound a)
+                                     (hiBound a)
+                                     (\ i -> gamma (index a i) 1)
+
 dirichlet :: (Lambda repr, Mochastic repr, Integrate repr) =>
               repr (Vector Prob) -> repr (Measure (Vector Prob))
-dirichlet a = (plate $ vector (loBound a)
-                             (hiBound a)
-                             (\ i -> gamma (index a i) 1)) `bind` \xs ->
+dirichlet a = unNormedDirichlet a `bind` \xs ->
               let_ (sumVec xs) (\normalized ->
               dirac $ vector (loBound xs)
                              (hiBound xs)
