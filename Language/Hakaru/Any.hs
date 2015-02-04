8a6d6a5accd09706ebb42f55b4a69752a50be76c
Chung-chieh Shan <ccshan@indiana.edu>
2015-02-04 13:50:41 -0500

    Add convenient Show instance for Any

--- Language/Hakaru/Any.hs
+++ Language/Hakaru/Any.hs
@@ -4,11 +4,16 @@
 module Language.Hakaru.Any (Any(Any, unAny), Any') where
 
 import Language.Hakaru.Syntax (Lambda, Mochastic, Integrate)
+import Language.Hakaru.PrettyPrint (runPrettyPrint)
 import Data.Typeable (Typeable)
 
 newtype Any a = Any { unAny :: Any' a }
   deriving Typeable
   -- beware GHC 7.8 https://ghc.haskell.org/trac/ghc/wiki/GhcKinds/PolyTypeable
+
+instance Show (Any a) where
+  show        (Any a) = show        (runPrettyPrint a)
+  showsPrec p (Any a) = showsPrec p (runPrettyPrint a)
 
 type Any' a =
   forall repr. (Mochastic repr, Integrate repr, Lambda repr) => repr a
