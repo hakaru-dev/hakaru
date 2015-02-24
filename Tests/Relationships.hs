1d2eb5907a0e58d3c966c8b9964090cf6f48cc87
Natalie Perna <pernanm@mcmaster.ca>
2015-02-24 13:02:37 -0500

    Simplify to gamma tests.

--- Tests/Relationships.hs
+++ Tests/Relationships.hs
@@ -14,6 +14,7 @@ testRelationships = test [
     "t2"   ~: testSS [t2] (lam (\beta -> gamma beta 2)),
     "t3"   ~: testSS [t3, t3'] (lam (\_ -> (lam (\beta -> gamma 2 beta)))),
     "t4"   ~: testSS [t4] (lam (\a -> lam (\b -> lam (\t -> beta a b)))),
+    "t5"   ~: testSS [t5, t5'] (lam (\alpha -> gamma 1 alpha)),
     "t7"   ~: testSS [t7] (normal 0 1 `bind` \x1 ->
                            normal 0 1 `bind` \x2 ->
                            dirac (x1 * recip x2)),
@@ -45,6 +46,12 @@ t4 = lam (\a -> lam (\b -> lam (\t ->
   gamma a t `bind` \x1 -> 
   gamma b t `bind` \x2 -> 
   dirac (x1/(x1+x2)))))
+
+t5 :: (Lambda repr, Mochastic repr) => repr (Prob -> Measure Prob)
+t5 = lam (\alpha -> uniform 0 1 `bind` \x -> dirac (-alpha * unsafeProb(log_ (unsafeProb x))))
+
+t5' :: (Lambda repr, Mochastic repr) => repr (Prob -> Measure Prob)
+t5' = lam (\alpha -> laplace (fromProb alpha) alpha `bind` \x -> dirac (abs (unsafeProb x)))
 
 t7 :: (Mochastic repr) => repr (Measure Real)
 t7 = cauchy 0 1
