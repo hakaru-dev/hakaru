3680d3f23188102944f7967f02cd5bdf62df2bc1
U-Yuriy-PC\Yuriy <toporoy@mcmaster.ca>
2015-03-25 03:11:03 -0400

    Add config hook to cabal to inform hint when the package is built in a sandbox, and use the compiled hakaru instead of interpretting.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,28 @@
 import Distribution.Simple
-main = defaultMain
+import Distribution.Simple.LocalBuildInfo
+import System.Directory
+
+extractSandboxes x = case foldr f [] x of 
+                       [] -> Nothing 
+                       r  -> Just r 
+ where f (SpecificPackageDB fp) = (fp :)
+       f _ = id 
+
+fileTemplate :: Maybe [String] -> String -> String 
+fileTemplate sandbox dir = unlines 
+  [ "module Language.Hakaru.Paths where"
+  , "sandboxPackageDB :: Maybe [String]"
+  , "sandboxPackageDB = " ++ show sandbox 
+  , "hakaruRoot :: String" 
+  , "hakaruRoot = " ++ show dir 
+  ]
+
+main = defaultMainWithHooks $ simpleUserHooks 
+  { postConf = \a b c d -> do 
+      let sb = extractSandboxes (withPackageDB d) 
+      cd <- getCurrentDirectory 
+      writeFile "Language/Hakaru/Paths.hs" (fileTemplate sb cd)
+      postConf simpleUserHooks a b c d 
+  }
+                         
+                       
