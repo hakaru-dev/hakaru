f2f84370162740325f881736fac8ae6325995e2b
Chung-chieh Shan <ccshan@indiana.edu>
2015-04-08 19:50:38 -0400

    Fix vector lengths and index-shifting

--- Tests/Vector.hs
+++ Tests/Vector.hs
@@ -19,8 +19,8 @@ allTests = test [ "testUnrolling" ~: testUnrolling
 testUnrolling :: Assertion
 testUnrolling = testSS [rolled] unrolled
 rolled, unrolled :: (Mochastic repr, Integrate repr) => repr (Measure Prob)
-rolled = liftM summateV (plate (vector 3 (\i ->
-         liftM unsafeProb (uniform 0 (fromInt i)))))
+rolled = liftM summateV (plate (vector 4 (\i ->
+         liftM unsafeProb (uniform 0 (2 + fromInt i)))))
 unrolled = uniform 0 2 `bind` \x2 ->
            uniform 0 3 `bind` \x3 ->
            uniform 0 4 `bind` \x4 ->
@@ -29,11 +29,11 @@ unrolled = uniform 0 2 `bind` \x2 ->
 
 -- Test that normalizing a vector makes its sum 1
 testNorm1, testNorm2 :: Assertion
-testNorm1 = testSS [liftM (summateV . normalizeV) (plate (vector 3 (\i ->
-                    liftM unsafeProb (uniform 0 (fromInt i)))))]
+testNorm1 = testSS [liftM (summateV . normalizeV) (plate (vector 4 (\i ->
+                    liftM unsafeProb (uniform 0 (2 + fromInt i)))))]
                    (dirac 1)
-testNorm2 = testSS [liftM summateV (dirichlet (vector 3 (\i ->
-                    unsafeProb (fromInt i))))]
+testNorm2 = testSS [liftM summateV (dirichlet (vector 4 (\i ->
+                    unsafeProb (2 + fromInt i))))]
                    (dirac 1)
 
 -- Test that the product of probability measures is a probability measure
@@ -42,7 +42,7 @@ testUnity = testSS [unity] count
 count, unity :: (Mochastic repr) => repr (Measure Int)
 count = categorical (vector 3 (\_ -> 1)) `bind` \i -> dirac (i * 10 + 20)
 unity = count `bind` \n ->
-        plate (vector n (\i -> bern (recip (unsafeProb (fromInt i))))) `bind_`
+        plate (vector n (\i -> bern (recip (unsafeProb (1 + fromInt i))))) `bind_`
         dirac n
 
 -- Test simplification of measure inside vector and product
@@ -50,17 +50,17 @@ testInside :: Assertion
 testInside = do testSS [      norm_nox_s]        norm_nox_s'
                 testSS [plate norm_nox_s] (plate norm_nox_s')
 norm_nox_s, norm_nox_s' :: (Mochastic repr) => repr (Vector (Measure Real))
-norm_nox_s  = vector 20 (\i -> normal (fromInt i) 1 `bind` \x ->
+norm_nox_s  = vector 21 (\i -> normal (10 + fromInt i) 1 `bind` \x ->
                                   normal x 1 `bind` \y ->
                                   dirac y)
-norm_nox_s' = vector 20 (\i -> normal (fromInt i) (sqrt_ 2))
+norm_nox_s' = vector 21 (\i -> normal (10 + fromInt i) (sqrt_ 2))
 
 -- Test pulling scalar factors out of product measure
 testPull :: Assertion
 testPull = testSS
-  [plate (vector 20 (\i -> weight (exp_ (fromInt i))
-                               $ normal (fromInt i) 1))]
-  (plate (vector 20 (\i -> normal (fromInt i) 1)))
+  [plate (vector 21 (\i -> weight (exp_ (fromInt i - 10))
+                               $ normal (fromInt i - 10) 1))]
+  (plate (vector 21 (\i -> normal (fromInt i - 10) 1)))
 
 -- Test conjugacy of dirichlet and categorical
 testConj :: Assertion
@@ -84,5 +84,5 @@ posterior as coin =
 testPlateDirac :: Assertion
 testPlateDirac = testSS [plateDirac] plateDirac'
 plateDirac, plateDirac' :: (Mochastic repr) => repr (Measure (Vector Real))
-plateDirac = plate (vector 10 (dirac . fromInt))
-plateDirac' = dirac (vector 10 fromInt)
+plateDirac = plate (vector 10 (dirac . (1+) . fromInt))
+plateDirac' = dirac (vector 10 ((1+) . fromInt))
