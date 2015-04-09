hakaru-dev/hakaru

commit d551a4dc7e7203f05517d33cd7ba08def2e6f328

simplify the translation of quant.  Use a more direct encoding, and also use gensym instead of escaped locals.  Makes debugging a lot easier.

Language/Hakaru/Maple.hs
@@ -139,7 +139,7 @@ instance Base Maple where
   vector    = quant "MVECTOR" 0
   empty     = Maple (return "MVECTOR(undefined,n=0..-1)")
   index     = mapleFun2 "index"
-  size      = mapleFun1 "size"
+  size      = mapleFun1 "size" -- should probably be "LinearAlgebra[Dimension]"
   reduce r z v = Maple (ReaderT $ \i -> return $
     "Reduce((" ++ (let x = "x" ++ show i
                        y = "x" ++ show (i+1)
@@ -151,11 +151,18 @@ instance Integrate Maple where
   integrate = quant "Int"
   summate   = quant "sum"
 
+-- use gensym rather than escaped locals.
+-- put lo and hi in directly, instead of passing them in.
+-- put the body in directly too, but still use a thunk for gensym
 quant :: String -> Maple b -> Maple b ->
          (Maple a -> Maple c) -> Maple d
-quant q lo hi f = mapleFun2 ("(proc (r,c) local x; "++q++"(c(x),x=r) end proc)")
-                            (mapleOp2 ".." lo hi)
-                            (lam f)
+quant q lo hi f = 
+  Maple (ReaderT $ \i -> return $ 
+    let lo' = runMaple lo i in
+    let hi' = runMaple hi i in
+    let body = runMaple (f (Maple (return "x"))) (i + 1) in
+    "(proc () local x; x := gensym(`h`);" ++
+        q ++ "(" ++ body ++",x=" ++ lo' ++ ".." ++ hi' ++") end proc)()")
 
 instance Lambda Maple where
   lam f = Maple (ReaderT $ \i -> return $

commit 5995738c7a3c20289074a01dd0ae5a348d3cd666

Test for hygiene
t69x is broken by d551a4dc7e7203f05517d33cd7ba08def2e6f328

Tests/RoundTrip.hs
@@ -52,7 +52,9 @@ testMeasureProb = test [
     "t49" ~: testSS [] t49,
     "t61" ~: testSS [t61] t61',
     "t66" ~: testSS [] t66,
-    "t67" ~: testSS [] t67
+    "t67" ~: testSS [] t67,
+    "t69x" ~: testSS [t69x] (dirac 1.5),
+    "t69y" ~: testSS [t69x] (dirac 3.5)
     ]
 
 testMeasureReal :: Test
@@ -653,6 +655,10 @@ t68 = lam $ \x4 ->
 
 t68' :: (Lambda repr, Mochastic repr) => repr (Prob -> Real -> Measure Real)
 t68' = lam $ \noise -> app (app t68 noise) noise
+
+t69x, t69y :: (Lambda repr, Mochastic repr, Integrate repr) => repr (Measure Prob)
+t69x = dirac (integrate 1 2 (\x -> integrate 3 4 (\y -> unsafeProb x)))
+t69y = dirac (integrate 1 2 (\x -> integrate 3 4 (\y -> unsafeProb y)))
 
 -- Testing round-tripping of some other distributions
 testexponential :: Mochastic repr => repr (Measure Prob)