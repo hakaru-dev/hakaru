aad4365bc910702d4580eb27e3cdd77c039a4768
Praveen Narayanan <pravnar@indiana.edu>
2015-05-29 20:35:36 -0400

    Extending comments on edge condition failures in Lazy

--- Language/Hakaru/Lazy.hs
+++ Language/Hakaru/Lazy.hs
@@ -628,14 +628,17 @@ instance (Mochastic repr, Lub repr) =>
   x ** y = Lazy
     (liftM2 (**) (forward x) (forward y))
     (\t -> lub (do r <- forward x
-                   -- if r is 0 or 1 then bot -- TODO
-                   (t',r') <- liftM2 (,) (atomize t) (atomize r)
+                   -- TODO if r is 0 or 1 then bot
+                   -- Maybe the (log r) in w takes care of the r=0 case
+                   t' <- atomize t
                    insert_ (ifTrue (less 0 t'))
                    w <- atomize (recip (abs (t * log r)))
                    insert_ (weight (unsafeProb w))
                    backward y (logBase r t))
                (do r <- forward y
-                   (t',r') <- liftM2 (,) (atomize t) (atomize r)
+                   -- TODO if r is 0 then bot
+                   -- Maybe the weight w takes care of this case
+                   t'<- atomize t
                    insert_ (ifTrue (less 0 t'))
                    let ex = t ** (recip r)
                    w <- atomize (abs (ex / (r * t)))
@@ -644,17 +647,14 @@ instance (Mochastic repr, Lub repr) =>
   logBase x y = Lazy
     (liftM2 logBase (forward x) (forward y))
     (\t -> lub (do r <- forward x
-                   r' <- atomize r
-                   insert_ (ifTrue (less 0 r'))
+                   -- TODO if r is 0 or 1 then bot
                    w <- atomize (abs ((r ** t) * log r))
                    insert_ (weight (unsafeProb w))
                    backward y (r ** t))
                (do r <- forward y
-                   (t',r') <- liftM2 (,) (atomize t) (atomize r)
-                   insert_ (ifTrue (or_ [and_ [      equal 0 t' ,equal 1 r']
-                                        ,and_ [not_ (equal 0 t'),less  0 r']]))
+                   -- TODO if r is 0 or 1 then bot
                    let ex = r ** (recip t)
-                   w <- atomize (abs (ex * (log r) / (t*t))) -- TODO div by 0
+                   w <- atomize (abs (ex * (log r) / (t*t)))
                    insert_ (weight (unsafeProb w))
                    backward x ex))
   sqrt x = Lazy
