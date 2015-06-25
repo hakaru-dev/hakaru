c6390eda26196acda2375afab86cbd805794d1f6
wren gayle romano <wren@community.haskell.org>
2015-06-24 21:25:52 -0400

    added NaryOps on lists, and uncommented the code using freshVar

--- Language/Hakaru/Syntax/Prelude.hs
+++ Language/Hakaru/Syntax/Prelude.hs
@@ -25,34 +25,38 @@
 module Language.Hakaru.Syntax.Prelude where
 
 -- import Prelude hiding (id, (.), Ord(..), Num(..), Integral(..), Fractional(..), Floating(..), Real(..), RealFrac(..), RealFloat(..), (^), (^^),.......)
-import Prelude (Maybe(..), Bool(..), Int, Double, Functor(..), flip, ($), undefined)
+import Prelude (Maybe(..), Bool(..), Int, Double, Functor(..), flip, ($), error, otherwise)
 import qualified Prelude
+import           Data.Sequence        (Seq)
 import qualified Data.Sequence        as Seq
--- import           Data.Proxy
+import           Data.Proxy
 import           Control.Category     (Category(..))
 import           Data.Number.LogFloat (LogFloat)
 
 import Language.Hakaru.Syntax.Nat
 import Language.Hakaru.Syntax.DataKind
+import Language.Hakaru.Syntax.TypeEq (SingI(sing))
 import Language.Hakaru.Syntax.AST
 import Language.Hakaru.Syntax.ABT hiding (View(..))
 
 ----------------------------------------------------------------
+----- Helper combinators for defining our EDSL
 {-
 Below we implement a lot of simple optimizations; however, these
 optimizations only apply if the client uses the type class methods
 to produce the AST. We should implement a stand-alone function which
 performs these sorts of optimizations, as a program transformation.
 -}
+-- TODO: constant propogation
 
 app :: (ABT abt) => abt ('HFun a b) -> abt a -> abt b
-app f x = syn $ App_ f x
+app = (syn .) . App_
 
 app2 :: (ABT abt) => abt ('HFun a ('HFun b c)) -> abt a -> abt b -> abt c
-app2 f x y = syn $ App_ (app f x) y
+app2 = (app .) . app
 
 app3 :: (ABT abt) => abt ('HFun a ('HFun b ('HFun c d))) -> abt a -> abt b -> abt c -> abt d
-app3 f x y z = syn $ App_ (app2 f x y) z
+app3 = (app2 .) . app
 
 primOp0_ :: (ABT abt) => PrimOp a -> abt a
 primOp0_ = syn . PrimOp_
@@ -66,27 +70,60 @@ primOp2_ = app2 . primOp0_
 primOp3_ :: (ABT abt) => PrimOp ('HFun a ('HFun b ('HFun c d))) -> abt a -> abt b -> abt c -> abt d
 primOp3_ = app3 . primOp0_
 
+-- TODO: generalize from [] to Foldable?
+unsafeNaryOp_ :: (ABT abt) => NaryOp a -> [abt a] -> abt a
+unsafeNaryOp_ o = go Seq.empty
+    where
+    go es []      = syn $ NaryOp_ o es -- N.B., @es@ may be empty!
+    go es (e:es') =
+        case matchNaryOp o e of
+        Nothing   -> go (es Seq.|> e)    es'
+        Just es'' -> go (es Seq.>< es'') es'
+
+-- TODO: generalize from [] to Foldable?
+naryOp_withIdentity :: (ABT abt) => NaryOp a -> abt a -> [abt a] -> abt a
+naryOp_withIdentity o i = go Seq.empty
+    where
+    go es []
+        | Seq.null es = i
+        | otherwise   = syn $ NaryOp_ o es
+    go es (e:es') =
+        case matchNaryOp o e of
+        Nothing   -> go (es Seq.|> e)    es'
+        Just es'' -> go (es Seq.>< es'') es'
+
 naryOp2_ :: (ABT abt) => NaryOp a -> abt a -> abt a -> abt a
 naryOp2_ o x y =
-    case (match x, match y) of
+    case (matchNaryOp o x, matchNaryOp o y) of
     (Just xs, Just ys) -> syn $ NaryOp_ o (xs Seq.>< ys)
     (Just xs, Nothing) -> syn $ NaryOp_ o (xs Seq.|> y)
     (Nothing, Just ys) -> syn $ NaryOp_ o (x  Seq.<| ys)
     (Nothing, Nothing) -> syn $ NaryOp_ o (x  Seq.<| Seq.singleton y)
-    where
-    match e =
-        caseVarSynABT e
-            (\_ _ -> Nothing)
-            $ \t  ->
-                case t of
-                NaryOp_ o' xs | o Prelude.== o' -> Just xs
-                _ -> Nothing
+
+matchNaryOp :: (ABT abt) => NaryOp a -> abt a -> Maybe (Seq (abt a))
+matchNaryOp o e =
+    caseVarSynABT e
+        (\_ _ -> Nothing)
+        $ \t  ->
+            case t of
+            NaryOp_ o' xs | o' Prelude.== o -> Just xs
+            _ -> Nothing
+
+
+-- TODO: give @k@ an actual @Var@ instead of the @Variable@ name? If we try that, then be sure to check 'uneither'
+-- TODO: how can we generate fresh names? Should we have @abt@ be a monad?
+freshVar :: (ABT abt) => (Variable -> abt a) -> abt a
+freshVar k = k $ error "TODO"
+
+
+----------------------------------------------------------------
+----- Now for the actual EDSL
 
 coerceTo_ :: (ABT abt) => Coercion a b -> abt a -> abt b
-coerceTo_ c = syn . CoerceTo_ c
+coerceTo_ = (syn .) . CoerceTo_
 
 unsafeFrom_ :: (ABT abt) => Coercion a b -> abt b -> abt a
-unsafeFrom_ c = syn . UnsafeFrom_ c
+unsafeFrom_ = (syn .) . UnsafeFrom_
 
 value_ :: (ABT abt) => Value a  -> abt a
 value_ = syn . Value_
@@ -101,14 +138,19 @@ prob_  = value_ . Prob_
 real_  :: (ABT abt) => Double   -> abt 'HReal
 real_  = value_ . Real_
 
+
 -- Boolean operators
 true, false :: (ABT abt) => abt 'HBool
 true  = bool_ True
 false = bool_ False
 
+-- TODO: simplifications: involution, distribution, constant-propogation
 not :: (ABT abt) => abt 'HBool -> abt 'HBool
 not = primOp1_ Not
 
+and, or :: (ABT abt) => [abt 'HBool] -> abt 'HBool
+and = naryOp_withIdentity And true
+or  = naryOp_withIdentity Or  false
 
 (&&), (||),
     -- (</=>), (<==>), (==>), (<==), (\\), (//)
@@ -127,31 +169,41 @@ nor    = primOp2_ Nor
 
 
 -- HEq & HOrder operators
-(==), (/=)
-    :: (ABT abt, HOrder a) => abt a -> abt a -> abt 'HBool
+(==), (/=) :: (ABT abt, HOrder a) => abt a -> abt a -> abt 'HBool
 (==) = primOp2_ Equal
 (/=) = (not .) . (==)
 
-(<), (<=), (>), (>=)
-    :: (ABT abt, HOrder a) => abt a -> abt a -> abt 'HBool
+(<), (<=), (>), (>=) :: (ABT abt, HOrder a) => abt a -> abt a -> abt 'HBool
 (<)    = primOp2_ Less
 x <= y = (x < y) || (x == y)
 (>)    = flip (<)
 (>=)   = flip (<=)
 
-min, max
-    :: (ABT abt, HOrder a) => abt a -> abt a -> abt a
+min, max :: (ABT abt, HOrder a) => abt a -> abt a -> abt a
 min = naryOp2_ Min
 max = naryOp2_ Max
+
+-- TODO: if @a@ is bounded, then we can make these safe...
+minimum, maximum :: (ABT abt, HOrder a) => [abt a] -> abt a
+minimum = unsafeNaryOp_ Min
+maximum = unsafeNaryOp_ Max
 
 -- N.B., we don't take advantage of commutativity, for more predictable AST outputs. However, that means we can end up being really slow;
 -- N.B., we also don't try to eliminate the identity elements or do cancellations because (a) it's undecidable in general, and (b) that's prolly better handled as a post-processing simplification step
 
 -- HSemiring operators
-(+), (*)
-    :: (ABT abt, HSemiring a) => abt a -> abt a -> abt a
+(+), (*) :: (ABT abt, HSemiring a) => abt a -> abt a -> abt a
 (+) = naryOp2_ Sum
 (*) = naryOp2_ Prod
+
+{-
+-- TODO
+zero, one :: (ABT abt, HSemiring a) => abt a
+
+sum, product :: (ABT abt, HSemiring a) => [abt a] -> abt a
+sum     = naryOp_withIdentity Sum  zero
+product = naryOp_withIdentity Prod one
+-}
 
 -- TODO: simplifications
 (^) :: (ABT abt, HSemiring a) => abt a -> abt 'HNat -> abt a
@@ -165,17 +217,14 @@ x - y = x + negate y
 -- BUG: can't just pattern match on (App_ (PrimOp_ Negate) e) anymore; can't even match on (App_ (Syn (PrimOp_ Negate)) e). We need to implement our AST-pattern matching stuff in order to clean this up...
 negate :: (ABT abt, HRing a) => abt a -> abt a
 negate e0 =
-    case me' of
-    Just e' -> e'
-    Nothing -> primOp1_ Negate e0 -- default case
-    where
-    me' =
-        caseVarSynABT e0
+    Prelude.maybe (primOp1_ Negate e0) id
+        $ caseVarSynABT e0
             (\_ _ -> Nothing)
             $ \t0 ->
                 case t0 of
-                NaryOp_ Sum xs -> Just . syn $ NaryOp_ Sum (fmap negate xs)
-                App_ f e       ->
+                NaryOp_ Sum xs ->
+                    Just . syn $ NaryOp_ Sum (fmap negate xs)
+                App_ f e ->
                     caseVarSynABT f
                         (\_ _ -> Nothing)
                         (\ft  ->
@@ -189,8 +238,15 @@ abs :: (ABT abt, HRing a) => abt a -> abt a
 abs = coerceTo_ signed . abs_
 
 abs_ :: (ABT abt, HRing a) => abt a -> abt (NonNegative a)
--- abs_ (CoerceTo_ (ConsCoercion Signed IdCoercion) e) = e -- BUG: we want to just return the @e@, but it's not AST...
-abs_ = primOp1_ Abs
+abs_ e =
+    Prelude.maybe (primOp1_ Abs e) id
+        $ caseVarSynABT e
+            (\_ _ -> Nothing)
+            $ \t  ->
+                case t of
+                CoerceTo_ (ConsCoercion Signed IdCoercion) e' -> Just e'
+                _ -> Nothing
+
 
 -- TODO: any obvious simplifications? idempotent?
 signum :: (ABT abt, HRing a) => abt a -> abt a
@@ -204,17 +260,14 @@ x / y = x * recip y
 -- TODO: generalize this pattern so we don't have to repeat it...
 recip :: (ABT abt, HFractional a) => abt a -> abt a
 recip e0 =
-    case me' of
-    Just e' -> e'
-    Nothing -> primOp1_ Recip e0 -- default case
-    where
-    me' =
-        caseVarSynABT e0
+    Prelude.maybe (primOp1_ Recip e0) id
+        $ caseVarSynABT e0
             (\_ _ -> Nothing)
             $ \t0 ->
                 case t0 of
-                NaryOp_ Prod xs -> Just . syn $ NaryOp_ Prod (fmap recip xs)
-                App_ f e        ->
+                NaryOp_ Prod xs ->
+                    Just . syn $ NaryOp_ Prod (fmap recip xs)
+                App_ f e ->
                     caseVarSynABT f
                         (\_ _ -> Nothing)
                         (\ft  ->
@@ -241,7 +294,7 @@ sqrt :: (ABT abt, HRadical a) => abt a -> abt a
 sqrt = (nat_ 2 `thRootOf`)
 
 {-
--- TODO: simplify
+-- TODO: simplifications
 (^+) :: (ABT abt, HRadical a) => abt a -> abt 'HPositiveRational -> abt a
 x ^+ y = casePositiveRational y $ \n d -> d `thRootOf` (x ^ n)
 
@@ -305,19 +358,15 @@ pair :: (ABT abt) => abt a -> abt b -> abt ('HPair a b)
 pair = primOp2_ Pair
 
 unpair
-    :: (ABT abt)
+    :: (ABT abt, SingI a, SingI b)
     => abt ('HPair a b)
     -> (abt a -> abt b -> abt c)
     -> abt c
-unpair = undefined
-{-
-unpair e f = do
-    x <- freshVar
-    y <- freshVar
-    return $ Case_ (syn e)
-        [(PPair PVar PVar,
-            open x (open y (syn $ f (var x sing) (var y sing)))]
--}
+unpair e f =
+    freshVar $ \x ->
+    freshVar $ \y ->
+    syn $ Case_ e
+        [(PPair PVar PVar, open x . open y $ f (var x sing) (var y sing))]
 
 inl :: (ABT abt) => abt a -> abt ('HEither a b)
 inl = primOp1_ Inl
@@ -326,20 +375,17 @@ inr :: (ABT abt) => abt b -> abt ('HEither a b)
 inr = primOp1_ Inr
 
 uneither
-    :: (ABT abt)
+    :: (ABT abt, SingI a, SingI b)
     => abt ('HEither a b)
     -> (abt a -> abt c)
     -> (abt b -> abt c)
     -> abt c
-uneither = undefined
-{-
-uneither e l r = do
-    x <- freshVar
-    return $ Case_ (syn e)
-        [ (PInl PVar, open x (syn $ l (var x sing)))
-        , (PInr PVar, open x (syn $ r (var x sing)))
+uneither e l r =
+    freshVar $ \x ->
+    syn $ Case_ e
+        [ (PInl PVar, open x $ l (var x sing))
+        , (PInr PVar, open x $ r (var x sing))
         ]
--}
 
 if_ :: (ABT abt) => abt 'HBool -> abt a -> abt a -> abt a
 if_ b t f = syn $ Case_ b [(PTrue, t), (PFalse, f)]
@@ -362,31 +408,25 @@ gammaFunc = primOp1_ GammaFunc
 betaFunc :: (ABT abt) => abt 'HProb -> abt 'HProb -> abt 'HProb
 betaFunc = primOp2_ BetaFunc
 
-fix :: (ABT abt) => (abt a -> abt a) -> abt a
-fix = undefined
-{-
-fix f = do
-    x <- freshVar
-    return $ Fix_ (open x (f (var x sing)))
--}
+fix :: (ABT abt, SingI a) => (abt a -> abt a) -> abt a
+fix f = 
+    freshVar $ \x ->
+    syn . Fix_ . open x $ f (var x sing)
 
--- TODO: rename @array@
+-- TODO: rename to @array@
 vector
     :: (ABT abt)
     => abt 'HInt
     -> (abt 'HInt -> abt a)
     -> abt ('HArray a)
-vector = undefined
-{-
-vector n f = do
-    x <- freshVar
-    return $ Array_ (syn $ unsafeFrom_ signed n) (open x (f (var x sing)))
--}
+vector n f =
+    freshVar $ \x ->
+    syn . Array_ (unsafeFrom_ signed n) . open x $ f (var x sing)
 
 empty :: (ABT abt) => abt ('HArray a)
 empty = primOp0_ Empty
 
--- TODO: rename @(!)@
+-- TODO: rename to @(!)@
 index :: (ABT abt) => abt ('HArray a) -> abt 'HInt -> abt a
 index xs i = primOp2_ Index xs (unsafeFrom_ signed i)
 
@@ -394,7 +434,7 @@ size :: (ABT abt) => abt ('HArray a) -> abt 'HInt
 size = coerceTo_ signed . primOp1_ Size
 
 reduce
-    :: (ABT abt)
+    :: (ABT abt, SingI a)
     => (abt a -> abt a -> abt a)
     -> abt a
     -> abt ('HArray a)
@@ -404,16 +444,13 @@ reduce f = primOp3_ Reduce (lam $ \x -> lam $ \y -> f x y)
 
 -- instance (ABT abt) => Mochastic (abt) where
 bind
-    :: (ABT abt)
+    :: (ABT abt, SingI a)
     => abt ('HMeasure a)
     -> (abt a -> abt ('HMeasure b))
     -> abt ('HMeasure b)
-bind = undefined
-{-
-bind e f = do
-    x <- freshVar
-    return $ Bind_ (syn e) (open x (f (var x sing)))
--}
+bind e f = 
+    freshVar $ \x ->
+    syn . Bind_ e . open x $ f (var x sing)
 
 dirac    :: (ABT abt) => abt a -> abt ('HMeasure a)
 dirac    = primOp1_ Dirac
@@ -571,12 +608,9 @@ integrate
     -> abt 'HReal
     -> (abt 'HReal -> abt 'HProb)
     -> abt 'HProb
-integrate = undefined
-{-
-integrate lo hi f = do
-    x <- freshVar
-    return $ Integrate_ (syn lo) (syn hi) (open x $ f (var x sing))
--}
+integrate lo hi f =
+    freshVar $ \x ->
+    syn . Integrate_ lo hi . open x $ f (var x sing)
 
 summate
     :: (ABT abt)
@@ -584,32 +618,23 @@ summate
     -> abt 'HReal
     -> (abt 'HInt -> abt 'HProb)
     -> abt 'HProb
-summate = undefined
-{-
-summate lo hi f = do
-    x <- freshVar
-    return $ Summate_ (syn lo) (syn hi) (open x $ f (var x sing))
--}
+summate lo hi f =
+    freshVar $ \x ->
+    syn . Summate_ lo hi . open x $ f (var x sing)
 
 
 -- instance (ABT abt) => Lambda abt where
 -- 'app' already defined
 
-lam :: (ABT abt) => (abt a -> abt b) -> abt ('HFun a b)
-lam = undefined
-{-
-lam f = do
-    x <- freshVar
-    return $ Lam_ Proxy (open x (f (var x sing)))
--}
+lam :: (ABT abt, SingI a) => (abt a -> abt b) -> abt ('HFun a b)
+lam f = 
+    freshVar $ \x ->
+    syn . Lam_ Proxy . open x $ f (var x sing)
 
-let_ :: (ABT abt) => abt a -> (abt a -> abt b) -> abt b
-let_ = undefined
-{-
+let_ :: (ABT abt, SingI a) => abt a -> (abt a -> abt b) -> abt b
 let_ e f = 
-    x <- freshVar
-    return $ Let_ (syn e) (open x (f (var x sing)))
--}
+    freshVar $ \x ->
+    syn . Let_ e . open x $ f (var x sing)
 
 -- instance (ABT abt) => Lub abt where
 lub :: (ABT abt) => abt a -> abt a -> abt a
