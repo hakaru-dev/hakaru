f3c99efa1da1184e6ec26e85c0cb72882be76218
wren gayle romano <wren@community.haskell.org>
2015-10-19 01:06:19 -0400

    cleaning up Language.Hakaru.Sampling.Sampler

--- Language/Hakaru/Sampling/Sampler.hs
+++ Language/Hakaru/Sampling/Sampler.hs
@@ -1,15 +1,30 @@
 {-# LANGUAGE RankNTypes #-}
-{-# OPTIONS -W #-}
 
-module Language.Hakaru.Sampling.Sampler (Sampler, deterministic, sbind, smap) where
+{-# OPTIONS_GHC -Wall -fwarn-tabs #-}
+----------------------------------------------------------------
+--                                                    2015.10.18
+-- |
+-- Module      :  Language.Hakaru.Sampling.Sampler
+-- Copyright   :  Copyright (c) 2015 the Hakaru team
+-- License     :  BSD3
+-- Maintainer  :  wren@community.haskell.org
+-- Stability   :  experimental
+-- Portability :  RankNTypes
+--
+-- TODO: is this still actually used anywhere? If so, we should eliminate the redundancy with "Language.Hakaru.Sampling.Metropolis"
+----------------------------------------------------------------
+module Language.Hakaru.Sampling.Sampler
+    ( Sampler, deterministic, sbind, smap
+    ) where
 
+import Data.Functor ((<$>))
+import Control.Monad.Primitive
 import Language.Hakaru.Sampling.Mixture (Mixture, mnull, empty, scale, point)
 import Language.Hakaru.Sampling.Distribution (choose)
 import Language.Hakaru.Sampling.Types
-import Control.Monad.Primitive
+----------------------------------------------------------------
 
--- Sampling procedures that produce one sample
-
+-- | Sampling procedures that produce one sample
 type Sampler a = (Functor m, PrimMonad m) => PRNG m -> m (Mixture a)
 
 deterministic :: Mixture a -> Sampler a
@@ -17,12 +32,17 @@ deterministic m _ = return m
 
 sbind :: Sampler a -> (a -> Sampler b) -> Sampler b
 sbind s k g = do
-  m1 <- s g
-  if mnull m1 then 
-      return empty
-  else do (a, v) <- choose m1 g
-          m2 <- k a g
-          return (scale v m2)
+    m1 <- s g
+    if mnull m1
+        then return empty
+        else do
+            (a, v) <- choose m1 g
+            scale v <$> k a g
 
 smap :: (a -> b) -> Sampler a -> Sampler b
-smap f s = sbind s (\a -> deterministic (point (f a) 1))
+smap f s =
+    s `sbind` \a ->
+    deterministic (point (f a) 1)
+
+----------------------------------------------------------------
+----------------------------------------------------------- fin.
