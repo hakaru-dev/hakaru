644f10b08293a521886ca243f416b070a41e0443
wren gayle romano <wren@community.haskell.org>
2015-10-22 04:40:43 -0400

    got DatumCase.hs to almost completely typeckeck

--- Language/Hakaru/Syntax/DatumCase.hs
+++ Language/Hakaru/Syntax/DatumCase.hs
@@ -26,18 +26,15 @@ module Language.Hakaru.Syntax.DatumCase
     -- * Helper types
       Some(..)
     , Pair1(..)
-    , DList1(..), runDList1, dnil1, dcons1, dappend1
+    , DList1(..), runDList1, toDList1, dnil1, dappend1
 
     -- * Reduction of case analysis
     , MatchResult(..)
-    , matchPatterns
-    , matchPattern
+    , matchBranches
+    , matchBranch
     ) where
 
-import Unsafe.Coerce (unsafeCoerce) -- TODO: move the stuff that uses this off to a separate file
-#if __GLASGOW_HASKELL__ < 710
-import Data.Monoid   hiding (Sum)
-#endif
+import Data.Proxy (Proxy(..)) -- TODO: Is this in Prelude for modern GHC?
 
 import Language.Hakaru.Syntax.IClasses
 import Language.Hakaru.Syntax.DataKind
@@ -78,8 +75,12 @@ append1 (Cons1 x xs) ys = Cons1 x (append1 xs ys)
 dnil1 :: DList1 a '[]
 dnil1 = DList1 id
 
-dcons1 :: a x -> DList1 a '[ x ]
-dcons1 x = DList1 (Cons1 x)
+-- HACK: we need to give this a top-level definition rather than
+-- inlining it in order to prove that the resulting index is @[x]@
+-- rather than possibly some other @(x:xs)@. No, I'm not sure why
+-- GHC can't infer that...
+dsingleton1 :: a x -> DList1 a '[ x ]
+dsingleton1 x = DList1 (Cons1 x)
 
 dappend1 :: DList1 a xs -> DList1 a ys -> DList1 a (xs ++ ys)
 dappend1 dx@(DList1 xs) dy@(DList1 ys) =
@@ -96,10 +97,10 @@ instance Show1 a => Show (DList1 a xs) where
     show      = show1
 
 instance JmEq1 a => JmEq1 (DList1 a) where
-    jmEq1 xs ys = 
-    
+    jmEq1 xs ys =
+
 instance Eq1 a => Eq1 (DList1 a) where
-    eq1 xs ys = 
+    eq1 xs ys =
 
 instance Eq1 a => Eq (DList1 a xs) where
     (==) = eq1
@@ -113,95 +114,131 @@ instance Foldable11 DList1 where
 
 ----------------------------------------------------------------
 data MatchResult :: ([Hakaru] -> Hakaru -> *) -> [Hakaru] -> Hakaru -> * where
-    MatchFail  :: MatchResult abt vars a
-    MatchStuck :: MatchResult abt vars a
+    -- TODO: actually store information inside GotStuck so we can
+    -- force the appropriate expression and continue without needing
+    -- to backtrack and redo what we've already done. (Of course,
+    -- until we factor @[Branch]@ into a single pattern automaton,
+    -- getting stuck in one branch doesn't tell us enough to avoid
+    -- restarting.)
+    --
+    -- | For when we encounter free variables and non-head-normal forms.
+    GotStuck :: MatchResult abt vars a
+
+    -- | We successfully matched everything (so far). The @vars2@
+    -- are for tracking variables bound by the future\/rest of the
+    -- pattern (i.e., for recursing into the left part of a product,
+    -- @vars2@ are the variables in the right part of the product).
     Matched
         :: DList1 (Pair1 Variable (abt '[])) vars1
         -> !(abt vars2 a)
-        -> MatchResult abt (vars1 ++ vars2) a
+        -> MatchResult abt vars2 a
 
 
-matchPatterns
+-- | Walk through a list of branches and try matching against them in order.
+matchBranches
     :: (ABT abt)
     => abt '[] a
     -> [Branch a abt b]
     -> Maybe (MatchResult abt '[] b)
-matchPatterns e bs0 = go id bs0
+matchBranches e = go
     where
-    go _  []     = Nothing
-    go ps (b:bs) =
-        case b of
-        Branch pat body ->
-            let  m = matchPattern e pat body in
-            case m of
-            MatchFail   -> go (ps . (b:)) bs
-            MatchStuck  -> Just m
-            Matched _ _ -> Just m
+    go []     = Nothing
+    go (b:bs) =
+        case matchBranch e b of
+        Nothing -> go bs
+        Just m  -> Just m
 
 
+-- | Try matching against a single branch.
+matchBranch
+    :: (ABT abt)
+    => abt '[] a
+    -> Branch a abt b
+    -> Maybe (MatchResult abt '[] b)
+matchBranch e (Branch pat body) =
+    case eqAppendNil (bodyToProxy body) of
+    Refl -> matchPattern e pat body
+    where
+    bodyToProxy :: abt xs a -> Proxy xs
+    bodyToProxy _ = Proxy
+
+
+-- | This function must be distinguished from 'matchBranch' since
+-- we allow nested patterns. If we enter this function from
+-- 'matchBranch' then we know @vars2@ must be @'[]@, but we also
+-- enter this function from 'matchFun' where @vars2@ could be
+-- anything! Thus, this function gives us the generalize inductive hypothesis needed to define 'matchBranch'.
 matchPattern
     :: (ABT abt)
     => abt '[] a
-    -> Pattern vars a
-    -> abt vars b
-    -> MatchResult abt vars b
+    -> Pattern vars1 a
+    -> abt (vars1 ++ vars2)  b
+    -> Maybe (MatchResult abt vars2 b)
 matchPattern e pat body =
     case pat of
-    PWild              -> Matched dnil1 body
+    PWild              -> Just (Matched dnil1 body)
     PVar               ->
         caseBind body $ \x body' ->
-            Matched (dcons1 (Pair1 x e)) body'
+            Just (Matched (dsingleton1 (Pair1 x e)) body')
     PDatum _hint1 pat1 ->
-        caseVarSyn e (const MatchStuck) $ \t ->
-            case t of
-            Value_ (VDatum (Datum _hint2 d)) ->
-                matchCode (fmap11 P.value_ d) pat1 body
-            Datum_         (Datum _hint2 d)  ->
-                matchCode d pat1 body
-            _                                -> MatchStuck
+        case viewDatum e of
+        Nothing               -> Just GotStuck
+        Just (Datum _hint2 d) -> matchCode d pat1 body
+
+
+-- HACK: we must give this a top-level binding rather than inlining it. Again, I'm not entirely sure why...
+viewDatum
+    :: (ABT abt)
+    => abt '[] (HData' t)
+    -> Maybe (Datum (abt '[]) (HData' t))
+viewDatum e =
+    caseVarSyn e (const Nothing) $ \t ->
+        case t of
+        Value_ (VDatum d) -> Just (fmap11 P.value_ d)
+        Datum_         d  -> Just d
+        _                 -> Nothing
 
 
 matchCode
     :: (ABT abt)
     => DatumCode  xss (abt '[]) (HData' t)
-    -> PDatumCode xss vars      (HData' t)
-    -> abt vars b
-    -> MatchResult abt vars b
+    -> PDatumCode xss vars1     (HData' t)
+    -> abt (vars1 ++ vars2)  b
+    -> Maybe (MatchResult abt vars2 b)
 matchCode (Inr d2) (PInr p2) body = matchCode   d2 p2 body
 matchCode (Inl d1) (PInl p1) body = matchStruct d1 p1 body
-matchCode _        _         _    = MatchFail
+matchCode _        _         _    = Nothing
 
 
 matchStruct
     :: (ABT abt)
     => DatumStruct  xs (abt '[]) (HData' t)
-    -> PDatumStruct xs vars      (HData' t)
-    -> abt vars b
-    -> MatchResult abt vars b
-matchStruct Done       PDone       body = Matched dnil1 body
-matchStruct (Et d1 d2) (PEt p1 p2) body =
-    error "TODO: matchStruct"
-    {-
-    case matchFun d1 p1 body of -- BUG: needs type coercion
-    MatchFail        -> MatchFail
-    MatchStuck       -> MatchStuck
-    Matched xs body' -> 
-        case matchStruct d2 p2 body' of -- BUG: needs type coercion
-        MatchFail         -> MatchFail
-        MatchStuck        -> MatchStuck
-        Matched ys body'' -> Matched (xs `dappend1` ys) body''
-    -}
-matchStruct _ _ _ = MatchFail
+    -> PDatumStruct xs vars1     (HData' t)
+    -> abt (vars1 ++ vars2)  b
+    -> Maybe (MatchResult abt vars2 b)
+matchStruct Done       PDone       body = Just (Matched dnil1 body)
+matchStruct (Et d1 d2) (PEt p1 p2) body = do
+    m1 <- error "TODO" -- matchFun d1 p1 body -- BUG: needs type coercion
+    case m1 of
+        GotStuck         -> return GotStuck
+        Matched xs body' -> do
+            m2 <- matchStruct d2 p2 body'
+            return $
+                case m2 of
+                GotStuck          -> GotStuck
+                Matched ys body'' -> Matched (xs `dappend1` ys) body''
+matchStruct _ _ _ = Nothing
+
 
 matchFun
     :: (ABT abt)
     => DatumFun  x (abt '[]) (HData' t)
-    -> PDatumFun x vars      (HData' t)
-    -> abt vars b
-    -> MatchResult abt vars b
+    -> PDatumFun x vars1     (HData' t)
+    -> abt (vars1 ++ vars2)  b
+    -> Maybe (MatchResult abt vars2 b)
 matchFun (Konst d2) (PKonst p2) body = matchPattern d2 p2 body
 matchFun (Ident d1) (PIdent p1) body = matchPattern d1 p1 body
-matchFun _           _          _    = MatchFail
+matchFun _           _          _    = Nothing
 
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.
