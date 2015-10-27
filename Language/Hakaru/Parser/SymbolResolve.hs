2574af173e62ec2865258e1980f334a13b707b02
Rob Zinkov <rob@zinkov.com>
2015-10-27 13:32:53 -0400

    first pass at symbolResolve

--- Language/Hakaru/Parser/SymbolResolve.hs
+++ Language/Hakaru/Parser/SymbolResolve.hs
@@ -1,35 +1,56 @@
 {-# LANGUAGE RankNTypes, GADTs, ExistentialQuantification, StandaloneDeriving #-}
 module Language.Hakaru.Parser.SymbolResolve where
 
+import Data.List
+import Data.Text
+
 import Language.Hakaru.Syntax.DataKind
-import Language.Hakaru.Syntax.AST (SCon, PrimOp, NaryOp)
-import Language.Hakaru.Parser.AST
+import qualified Language.Hakaru.Syntax.AST as T
+import qualified Language.Hakaru.Parser.AST as U
+import Language.Hakaru.Syntax.Sing
+import Language.Hakaru.Syntax.IClasses
+import qualified Language.Hakaru.Syntax.Nat as N
 
 data Symbol' =
-     Fix
-   | True' | False'
-   | CoerceTo
-   | UnsafeFrom 
-   | PrimOp
-   | NaryOp
-   | Array
-   | Empty'
-   | MeasureOp'
-   | MBind
+     Pair_
+   | True_ | False_
+   | FromProb_
+   | UnsafeProb_ 
+   | Uniform_
+   | Normal_
+   | VarSym U.Name
    | Plus
 
 type SymbolTable = [(Text, Symbol')]
 
 primTable :: [(Text, Symbol')]
-primTable =  undefined
+primTable =  [("Pair",       Pair_)
+             ,("True",       True_)
+             ,("False",      False_)
+             ,("fromProb",   FromProb_)
+             ,("unsafeProb", UnsafeProb_)
+             ,("uniform",    Uniform_)
+             ,("normal",     Normal_)
+             ,("+",          Plus)
+             ]
 
 -- figure out symbols and types
-symbolResolution :: SymbolTable -> AST' Text -> AST' Symbol
-symbolResolution = undefined
+symbolResolution :: SymbolTable -> U.AST' Text -> U.AST' Symbol
+symbolResolution symbols ast =
+    case ast of
+      U.Var name -> case lookup name symbols of
+                      Nothing -> VarSym (U.Name name $ N.unsafeNat 0)
+                      Just a  -> a
+      U.App f x  -> U.App (symbolResolution f)
+                          (symbolResolution f)
+      _          -> error "TODO: Add rest of cases"
 
 -- make AST and give unique names for variables
-makeAST :: AST' Symbol' -> AST Symbol'
-makeAST = undefined
+makeAST :: U.AST' Symbol' -> U.AST Symbol'
+makeAST ast =
+    case ast of
+      -- U.Var Normal_ -> (\a b -> U.SealedOp T.Normal)
+      _             -> error "TODO: Add rest of cases"
 
 data PrimOp' =
      Not'        
