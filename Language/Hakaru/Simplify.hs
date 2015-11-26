0a18e22a58f83ad4787add892aabf10249ad37f0
Rob Zinkov <rob@zinkov.com>
2015-11-26 16:50:35 -0500

    Begin to change Simplify to use NewSLO and AST

--- Language/Hakaru/Simplify.hs
+++ Language/Hakaru/Simplify.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE TypeSynonymInstances
            , FlexibleInstances
+           , FlexibleContexts
            , DeriveDataTypeable
            , CPP
+           , GADTs
+           , DataKinds
            , OverloadedStrings
            , ScopedTypeVariables
            #-}
@@ -20,46 +23,45 @@
 -- Take strings from Maple and interpret them in Haskell (Hakaru)
 ----------------------------------------------------------------
 module Language.Hakaru.Simplify
-    ( closeLoop
-    , simplify
+    ( simplify
     , toMaple
     , openLoop
     , main
     , Simplifiable(mapleType)
     , MapleException(MapleException)
-    , InterpreterException(InterpreterException)
     ) where
 
 import Control.Exception
+
 import Language.Hakaru.Simplifiable (Simplifiable(mapleType))
-import Language.Hakaru.Maple (Maple, runMaple)
+import Language.Hakaru.MapleNeue (Maple, runMaple)
 import Language.Hakaru.Any (Any(Any), AnySimplifiable(AnySimplifiable))
 import Language.Hakaru.PrettyPrint (pretty)
+
+import Language.Hakaru.Parser.Maple
+import Language.Hakaru.Parser.SymbolResolve (resolveAST)
+
+import Language.Hakaru.Syntax.ABT
+import Language.Hakaru.Syntax.AST
+import Language.Hakaru.Syntax.DataKind
+import Language.Hakaru.Syntax.IClasses
+import Language.Hakaru.Syntax.TypeCheck
+import Language.Hakaru.Syntax.TypeOf
+
+import Data.Typeable (Typeable)
+
+import Control.Monad.Trans.State.Strict (evalState)
+
 import System.IO (stderr, hPrint, hPutStrLn)
-import Data.Typeable (Typeable, typeOf)
 import Data.List (tails, stripPrefix)
 import Data.Text (replace, pack, unpack)
 import Data.Char (isSpace)
 import System.MapleSSH (maple)
-import Language.Haskell.Interpreter.Unsafe (unsafeRunInterpreterWithArgs)
-import Language.Haskell.Interpreter (
-#ifdef PATCHED_HINT
-    unsafeInterpret,
-#else
-    interpret,
-#endif
-    InterpreterError(WontCompile), GhcError(GhcError),
-    MonadInterpreter, set, get, OptionVal((:=)),
-    searchPath, languageExtensions, Extension(UnknownExtension),
-    loadModules, setImports)
 
 import Language.Hakaru.Util.Lex (readMapleString)
-import Language.Hakaru.Paths
 ----------------------------------------------------------------
 
 data MapleException       = MapleException String String
-    deriving Typeable
-data InterpreterException = InterpreterException InterpreterError String
     deriving Typeable
 
 -- Maple prints errors with "cursors" (^) which point to the specific position
@@ -70,103 +72,44 @@ instance Show MapleException where
         "MapleException:\n" ++ fromMaple ++
         "\nafter sending to Maple:\n" ++ toMaple_
 
-instance Show InterpreterException where
-    show (InterpreterException (WontCompile es) cause) =
-        "InterpreterException:\n" ++ unlines [ msg | GhcError msg <- es ] ++
-        "\nwhile interpreting:\n" ++ cause
-    show (InterpreterException err cause) =
-        "InterpreterException:\n" ++ show err ++
-        "\nwhile interpreting:\n" ++ cause
-
 instance Exception MapleException
 
-instance Exception InterpreterException
-
-ourGHCOptions, ourSearchPath :: [String]
-ourGHCOptions =
-    case sandboxPackageDB of
-    Nothing -> []
-    Just xs -> "-no-user-package-db" : map ("-package-db " ++) xs
-
-ourSearchPath = [ hakaruRoot ]
-
-ourContext :: MonadInterpreter m => m ()
-ourContext = do
-    let modules = [ "Tests.Imports", "Tests.EmbedDatatypes" ]
-    
-    set [ searchPath := ourSearchPath ]
-    
-    loadModules modules
-    
-    -- "Tag" requires DataKinds to use type list syntax
-    exts <- get languageExtensions
-    set [ languageExtensions := (UnknownExtension "DataKinds" : exts) ]
-    
-    setImports modules
-
-
--- Type checking is fragile for this function. It compiles fine
--- from the commandline, but using `cabal repl` causes it to break
--- due to OverloadedStrings and (supposed) ambiguity about @a@ in
--- the Typeable constraint. I've patched those two issues up by
--- giving an explicit type signature to @s'@, and using ScopedTypeVariables
--- to form the argument to 'typeOf' (rather than using 'asTypeOf'-style
--- tricks). Then again, 'simplify' doesn't work inside `cabal repl`
--- anyways, due to some dependency packages being hidden for some
--- odd reason...
-closeLoop :: forall a. (Typeable a) => String -> IO a
-closeLoop s = do
-    result <- unsafeRunInterpreterWithArgs ourGHCOptions $ do
-        ourContext
-#ifdef PATCHED_HINT
-        unsafeInterpret s' typeStr
-#else
-        interpret s' undefined
-#endif
-    case result of
-        Left err -> throw (InterpreterException err s')
-        Right a -> return a
-    where
-    s' :: String
-    s' = s ++ " :: " ++ typeStr
-
-    typeStr = unpack
-        $ replace ":" "Cons"
-        $ replace "[]" "Nil"
-        $ pack (show (typeOf (undefined :: a)))
-
-mkTypeString :: (Simplifiable a) => String -> proxy a -> String
-mkTypeString s t = "Typed(" ++ s ++ ", " ++ mapleType t ++ ")"
-
-simplify :: (Simplifiable a) => Maple a -> IO (Any a)
+simplify :: forall abt a. (ABT AST abt) => abt '[] a -> IO (abt '[] a)
 simplify e = do
     hakaru <- simplify' e
-    closeLoop ("Any (" ++ hakaru ++ ")")
+    TypedAST typ ast <- closeLoop' hakaru
+    -- TODO: convince Haskell I can return ast
+    return e
 
-simplify' :: (Simplifiable a) => Maple a -> IO String
-simplify' e = do
-    let slo = toMaple e
-    hopeString <- maple ("timelimit(15,Haskell(SLO:-AST(SLO(" ++ slo ++ "))));")
-    case readMapleString hopeString of
-        Just hakaru -> return hakaru
-        Nothing     -> throw (MapleException slo hopeString)
+ where simplify' :: abt '[] a -> IO String
+       simplify' e = do
+         let slo = toMaple e
+         hopeString <- maple ("timelimit(15,NewSLO:-RoundTripLO(" ++ slo ++ "));")
+         case readMapleString hopeString of
+           Just hakaru -> return hakaru
+           Nothing     -> throw (MapleException slo hopeString)
+                          
+       closeLoop' :: String -> IO (TypedAST abt)
+       closeLoop' s = do
+           case parseMaple (pack s) of
+             Left err -> error (show err)
+             Right past ->
+                 let m = inferType (resolveAST $ maple2AST past) in
+                 case runTCM m LaxMode of
+                   Left err -> error (show err)
+                   Right e  -> return e
 
-toMaple :: (Simplifiable a) => Maple a -> String
-toMaple e = mkTypeString (runMaple e 0) e
+toMaple :: (ABT AST abt) => abt '[] a -> String
+toMaple = runMaple 
 
 main :: IO ()
-main = action `catch` handler1 `catch` handler0 where
+main = action `catch` handler0 where
     action :: IO ()
     action = do
         s <- readFile "/tmp/t" -- getContents
         let (before, middle, after) = trim s
-        middle' <- simplifyAny middle
+        middle' <- undefined -- simplifyAny middle
         putStr (before ++ middle' ++ after)
-
-    handler1 ::  InterpreterError -> IO ()
-    handler1 (WontCompile es) =
-        sequence_ [ hPutStrLn stderr msg | GhcError msg <- es ]
-    handler1 exception = throw exception
 
     handler0 :: SomeException -> IO ()
     handler0 = hPrint stderr
@@ -177,27 +120,9 @@ trim s =
         (after', middle') = span isSpace (reverse s')
     in (before, reverse middle', reverse after')
 
-simplifyAny :: String -> IO String
-simplifyAny s = do
-    (names, AnySimplifiable e) <- openLoop [] s
-    Any e' <- simplify e
-    return (show (pretty e')) -- BUG: the old version used @runPrettyPrintNamesPrec e' names 0@, but the new prettyprinter doesn't have provisions for name supplies since it doesn't need them; thus there are almost certainly hygiene issues here.
-
 openLoop :: [String] -> String -> IO ([String], AnySimplifiable)
 openLoop names s =
-    fmap ((,) names) (closeLoop ("AnySimplifiable (" ++ s ++ ")")) `catch` h
-  where
-    h :: InterpreterException -> IO ([String], AnySimplifiable)
-    h (InterpreterException (WontCompile es) _)
-      | not (null unbound) && not (any (`elem` names) unbound)
-      = openLoop (unbound ++ names) (unlines header ++ s)
-      where unbound = [ init msg''
-                      | GhcError msg <- es
-                      , msg' <- tails msg
-                      , Just msg'' <- [stripPrefix ": Not in scope: `" msg']
-                      , last msg'' == '\'' ]
-            header = [ "lam $ \\" ++ name ++ " ->" | name <- unbound ]
-    h (InterpreterException exception _) = throw exception
+    fmap ((,) names) (undefined {- closeLoop ("AnySimplifiable (" ++ s ++ ")") -})
 
 ----------------------------------------------------------------
 ----------------------------------------------------------- fin.
--- Setup.hs
+++ Setup.hs
@@ -1,28 +1,2 @@
 import Distribution.Simple
-import Distribution.Simple.LocalBuildInfo
-import System.Directory
-
-extractSandboxes x = case foldr f [] x of 
-                       [] -> Nothing 
-                       r  -> Just r 
- where f (SpecificPackageDB fp) = (fp :)
-       f _ = id 
-
-fileTemplate :: Maybe [String] -> String -> String 
-fileTemplate sandbox dir = unlines 
-  [ "module Language.Hakaru.Paths where"
-  , "sandboxPackageDB :: Maybe [String]"
-  , "sandboxPackageDB = " ++ show sandbox 
-  , "hakaruRoot :: String" 
-  , "hakaruRoot = " ++ show dir 
-  ]
-
-main = defaultMainWithHooks $ simpleUserHooks 
-  { postConf = \a b c d -> do 
-      let sb = extractSandboxes (withPackageDB d) 
-      cd <- getCurrentDirectory 
-      writeFile "Language/Hakaru/Paths.hs" (fileTemplate sb cd)
-      postConf simpleUserHooks a b c d 
-  }
-                         
-                       
+main = defaultMain
