14f4acc07a37f8fc8c5f6d37061d8fc1e273edc6
Rob Zinkov <rob@zinkov.com>
2015-12-14 12:36:28 -0500

    Add comment to Observe

--- Language/Hakaru/Observe.hs
+++ Language/Hakaru/Observe.hs
@@ -3,6 +3,25 @@
            , FlexibleContexts
            #-}
 
+{-# OPTIONS_GHC -Wall -fwarn-tabs #-}
+----------------------------------------------------------------
+--                                                    2015.12.14
+-- |
+-- Module      :  Language.Hakaru.Observe
+-- Copyright   :  Copyright (c) 2015 the Hakaru team
+-- License     :  BSD3
+-- Maintainer  :  ppaml@indiana.edu
+-- Stability   :  experimental
+-- Portability :  GHC-only
+--
+-- A simpler version of the work done in 'Language.Hakaru.Disintegrate'
+--
+-- In principle, this module's functionality is entirely subsumed
+-- by the work done in Language.Hakaru.Disintegrate, so we can hope
+-- to define observe in terms of disintegrate. This is still useful
+-- as a guide to those that want something more in line with what other
+-- probabilisitc programming systems support.
+----------------------------------------------------------------
 module Language.Hakaru.Observe where
 
 import Language.Hakaru.Syntax.AST
