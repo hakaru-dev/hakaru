6df420357b829c5b822704b9f0202e6132a7c5d8
Rob Zinkov <rob@zinkov.com>
2015-12-19 19:55:17 -0500

    Add types to emacs highlighting

--- tools/hakaru.el
+++ tools/hakaru.el
@@ -2,17 +2,18 @@
 (setq myKeywords
  '(("#.*$" . font-lock-comment-face)
    ("return\\|True\\|False" . font-lock-constant-face)
+   ("nat\\|int\\|prob\\|real" . font-lock-type-face)
    ("\\(\\w+\\)\\s-*\(" . (1 font-lock-function-name-face))		
    ("\"\\.\\*\\?" . font-lock-string-face)		
        ;; ; : , ; { } =>  @ $ = are all special elements
    ("def\\|fn\\|if\\|else\\|\sin\s"
     . font-lock-keyword-face)
-   ("\\([~^<>=.\\+*/%-]\\)" 0 'font-lock-variable-name-face))
+   ("\\([~^<>=.\\+*/%-]\\)" 0 'font-lock-variable-name-face)))
 
-(define-derived-mode bruno-lang-mode fundamental-mode
+(define-derived-mode hakaru-lang-mode fundamental-mode
   (setq font-lock-defaults '(myKeywords))
   (setq mode-name "Hakaru"))
 
-(autoload 'bruno-lang-mode "Hakaru" nil t)
-(add-to-list 'auto-mode-alist '("\\.hk$" . bruno-lang-mode))
+(autoload 'hakaru-lang-mode "Hakaru" nil t)
+(add-to-list 'auto-mode-alist '("\\.hk$" . hakaru-lang-mode))
 
