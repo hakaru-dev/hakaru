d1be645d89906f4a636df2f4bf7b5457f335c83f
Rob Zinkov <rob@zinkov.com>
2015-12-27 20:23:24 -0500

    Adding Lebesgue to Pretty.Maple

--- haskell/Language/Hakaru/Pretty/Maple.hs
+++ haskell/Language/Hakaru/Pretty/Maple.hs
@@ -89,10 +89,11 @@ wmtom (w, m) = app2 "Weight" w m
 mapleMeasureOp
     :: (ABT Term abt, typs ~ UnLCs args, args ~ LCs typs)
     => MeasureOp typs a -> SArgs abt args -> String
-mapleMeasureOp Uniform (e1 :* e2 :* End) = app2 "Uniform"  e1 e2
-mapleMeasureOp Normal  (e1 :* e2 :* End) = app2 "Gaussian" e1 e2
-mapleMeasureOp Gamma   (e1 :* e2 :* End) = app2 "GammaD"   e1 e2
-mapleMeasureOp Beta    (e1 :* e2 :* End) = app2 "BetaD"    e1 e2
+mapleMeasureOp Lebesgue End               = "Lebesgue()"
+mapleMeasureOp Uniform  (e1 :* e2 :* End) = app2 "Uniform"  e1 e2
+mapleMeasureOp Normal   (e1 :* e2 :* End) = app2 "Gaussian" e1 e2
+mapleMeasureOp Gamma    (e1 :* e2 :* End) = app2 "GammaD"   e1 e2
+mapleMeasureOp Beta     (e1 :* e2 :* End) = app2 "BetaD"    e1 e2
 
 mapleType :: Sing (a :: Hakaru) -> String
 mapleType SNat         = "Nat"
