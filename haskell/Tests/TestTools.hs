e1415d5810f17fb6405ad7265a4a915a347a12c7
Rob Zinkov <rob@zinkov.com>
2016-01-04 15:21:49 -0500

    Make TestTools typecheck

--- haskell/Tests/TestTools.hs
+++ haskell/Tests/TestTools.hs
@@ -2,27 +2,12 @@
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
 module Tests.TestTools where
 
--- import Language.Hakaru.Syntax (Measure, Lambda(lam), Order_)
-import Language.Hakaru.Maple (Maple, runMaple)
-import Language.Hakaru.Simplify (simplify, Simplifiable)
-import Language.Hakaru.Any (Any(unAny), Any')
-import Language.Hakaru.PrettyPrint (PrettyPrint, runPrettyPrint, leftMode)
-import Text.PrettyPrint (Doc)
 import Data.Maybe (isJust)
 import Data.List
-import Data.Function (on)
 import Data.Typeable (Typeable)
 import Control.Exception
 
 import Test.HUnit
-
-newtype Result = Result Doc
-result :: PrettyPrint a -> Result
-result = Result . runPrettyPrint
-instance Eq Result where Result a == Result b = leftMode a == leftMode b
-instance Show Result where
-  showsPrec 0 (Result a) = showChar '\n' . showsPrec 0 a
-  showsPrec _ (Result a) =                 showsPrec 0 a
 
 data TestException = TestSimplifyException String SomeException
     deriving Typeable
@@ -39,31 +24,28 @@ assertJust :: Maybe a -> Assertion
 assertJust = assertBool "expected Just but got Nothing" . isJust
 
 -- Assert that a given Hakaru program roundtrips (aka simplifies) without error
-testS :: (Simplifiable a) => Any' a -> Assertion
-testS t = do
-    p <- simplify t `catch` handleSimplify t
-    let s = result (unAny p)
-    assertResult (show s)
+-- testS :: (Simplifiable a) => Any' a -> Assertion
+-- testS t = do
+--     p <- simplify t `catch` handleSimplify t
+--     let s = result (unAny p)
+--     assertResult (show s)
 
 -- Assert that all the given Hakaru programs simplify to the given one
-testSS :: (Simplifiable a) => [Maple a] -> Any' a -> Assertion
-testSS ts t' =
-    mapM_ (\t -> do p <- simplify t --`catch` handleSimplify t
-                    (assertEqual "testSS" `on` result) t' (unAny p))
-          (t' : ts)
+-- testSS :: (Simplifiable a) => [Maple a] -> Any' a -> Assertion
+-- testSS ts t' =
+--     mapM_ (\t -> do p <- simplify t --`catch` handleSimplify t
+--                     (assertEqual "testSS" `on` result) t' (unAny p))
+--           (t' : ts)
 
-handleSimplify :: PrettyPrint a -> SomeException -> IO (Any a)
-handleSimplify t e = throw (TestSimplifyException (show t) e)
+-- testMaple :: Maple a -> IO ()
+-- testMaple t = assertResult $ runMaple t 0
 
-testMaple :: Maple a -> IO ()
-testMaple t = assertResult $ runMaple t 0
-
-testMapleEqual :: Maple a -> Maple a -> IO ()
-testMapleEqual t1 t2 = do
-    let r1 = rm t1
-    let r2 = rm t2
-    assertEqual "testMapleEqual: false" r1 r2
-    where rm t = runMaple t 0
+-- testMapleEqual :: Maple a -> Maple a -> IO ()
+-- testMapleEqual t1 t2 = do
+--     let r1 = rm t1
+--     let r2 = rm t2
+--     assertEqual "testMapleEqual: false" r1 r2
+--     where rm t = runMaple t 0
 
 ignore :: a -> Assertion
 ignore _ = assertFailure "ignored"  -- ignoring a test reports as a failure
