6ca1eae0c175b6f9d7184cd509a730b05264bd15
Rob Zinkov <rob@zinkov.com>
2016-01-23 10:49:54 -0500

    Remove nested annotations pass

--- haskell/Language/Hakaru/Normalize.hs
+++ haskell/Language/Hakaru/Normalize.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds
+           , GADTs
            , FlexibleContexts 
            #-}
 
@@ -9,7 +10,15 @@ import Language.Hakaru.Syntax.AST
 
 
 removeNestedAnnotations :: ABT Term abt => abt '[] a -> abt '[] a
-removeNestedAnnotations  = cataABT var bind syn
+removeNestedAnnotations  = cataABT var bind $ \t1 ->
+                             case t1 of
+                               Ann_ typ :$ e1 :* End ->
+                                   caseVarSyn e1 var $ \t2 ->
+                                       case t2 of
+                                         Ann_ _ :$ e2 :* End ->
+                                              syn (Ann_ typ :$ e2 :* End)
+                                         _ -> syn t1 
+                               x -> syn x
 
 collapseNestedSuperposes :: ABT Term abt => abt '[] a -> abt '[] a
 collapseNestedSuperposes = cataABT var bind syn
