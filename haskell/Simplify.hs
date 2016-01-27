980985c8193c726497c23802a8f22cd1e933fd1c
Rob Zinkov <rob@zinkov.com>
2016-01-27 01:04:17 -0500

    Added simplify command

---
+++ haskell/Simplify.hs
@@ -0,0 +1,46 @@
+{-# LANGUAGE OverloadedStrings, DataKinds, GADTs #-}
+
+module Main where
+
+import qualified Language.Hakaru.Parser.AST as U
+import           Language.Hakaru.Parser.Parser
+import           Language.Hakaru.Parser.SymbolResolve (resolveAST)
+import           Language.Hakaru.Pretty.Concrete  
+import qualified Language.Hakaru.Syntax.AST as T
+import           Language.Hakaru.Syntax.ABT
+import           Language.Hakaru.Syntax.TypeCheck
+import           Language.Hakaru.Syntax.Prelude (prob_, fromProb, real_)
+
+import           Language.Hakaru.Types.Sing
+import           Language.Hakaru.Simplify
+  
+import           Control.Monad
+import           Data.Text
+import qualified Data.Text.IO as IO
+
+import           System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+      [prog] -> IO.readFile prog >>= runSimplify
+      []     -> IO.getContents   >>= runSimplify
+      _      -> IO.putStrLn "Usage: simplify <file>"
+
+inferType' :: U.AST -> TypeCheckMonad (TypedAST (TrivialABT T.Term))
+inferType' = inferType
+
+runSimplify :: Text -> IO ()
+runSimplify prog =
+    case parseHakaru prog of
+    Left  err  -> print err
+    Right past ->
+        let m = inferType' (resolveAST past) in
+        case runTCM m LaxMode of
+        Left err                 -> putStrLn err
+        Right (TypedAST typ ast) -> do
+          case typ of
+            SMeasure _ -> simplify ast >>= print . pretty
+            _          -> print (pretty ast)
+
