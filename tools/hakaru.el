5d19a069ced17231bf00e86c512f13e0a09a2335
Rob Zinkov <rob@zinkov.com>
2016-01-27 13:30:26 -0500

    Make syntax highlight better

--- tools/hakaru.el
+++ tools/hakaru.el
@@ -2,13 +2,13 @@
 (setq myKeywords
  '(("#.*$" . font-lock-comment-face)
    ("return\\|True\\|False" . font-lock-constant-face)
-   ("nat\\|int\\|prob\\|real" . font-lock-type-face)
    ("\\(\\w+\\)\\s-*\(" . (1 font-lock-function-name-face))		
    ("\"\\.\\*\\?" . font-lock-string-face)		
        ;; ; : , ; { } =>  @ $ = are all special elements
    ("def\\|fn\\|if\\|else\\|\sin\s"
     . font-lock-keyword-face)
-   ("\\([~^<>=.\\+*/%-]\\)" 0 'font-lock-variable-name-face)))
+   ("\\([~^<>=.\\+*/%-]\\)" 0 'font-lock-variable-name-face)
+   ("nat\\|int\\|prob\\|real" . font-lock-type-face)))
 
 (define-derived-mode hakaru-lang-mode fundamental-mode
   (setq font-lock-defaults '(myKeywords))
