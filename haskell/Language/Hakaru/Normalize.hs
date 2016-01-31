f43e7e0db403bbd035e4fcbbdcc033dd0b275959
Rob Zinkov <rob@zinkov.com>
2016-01-31 15:14:48 -0500

    Add removeTypeAnnotation

--- haskell/Language/Hakaru/Normalize.hs
+++ haskell/Language/Hakaru/Normalize.hs
@@ -9,19 +9,27 @@ import Language.Hakaru.Syntax.ABT
 import Language.Hakaru.Syntax.AST
 
 
-removeNestedAnnotations :: ABT Term abt => abt '[] a -> abt '[] a
-removeNestedAnnotations  = cataABT var bind $ \t1 ->
-                             case t1 of
-                               Ann_ typ :$ e1 :* End ->
-                                   caseVarSyn e1 var $ \t2 ->
-                                       case t2 of
-                                         Ann_ _ :$ e2 :* End ->
-                                              syn (Ann_ typ :$ e2 :* End)
-                                         _ -> syn t1 
-                               x -> syn x
+removeNestedTypeAnnotations :: ABT Term abt => abt '[] a -> abt '[] a
+removeNestedTypeAnnotations  =
+    cataABT var bind $ \t1 ->
+        case t1 of
+        Ann_ typ :$ e1 :* End ->
+             caseVarSyn e1 var $ \t2 ->
+                 case t2 of
+                 Ann_ _ :$ e2 :* End ->
+                      syn (Ann_ typ :$ e2 :* End)
+                 _ -> syn t1 
+        x -> syn x
 
 collapseNestedSuperposes :: ABT Term abt => abt '[] a -> abt '[] a
 collapseNestedSuperposes = cataABT var bind syn
 
+stripTypeAnnotations :: ABT Term abt => abt '[] a -> abt '[] a
+stripTypeAnnotations =
+    cataABT var bind $ \t1 ->
+        case t1 of
+        Ann_ typ :$ e1 :* End -> e1
+        x -> syn x
+
 reduceAST :: ABT Term abt => abt '[] a -> abt '[] a
-reduceAST = removeNestedAnnotations . collapseNestedSuperposes
+reduceAST = removeNestedTypeAnnotations . collapseNestedSuperposes
