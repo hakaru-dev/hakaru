8d0fd3c122f6b45910419222e8ef73ca485faf0d
Rob Zinkov <rob@zinkov.com>
2016-02-18 15:39:22 -0500

    Adding test file for runtime

---
+++ haskell/Language/Hakaru/Runtime/NormTest.hs
@@ -0,0 +1,15 @@
+--module Language.Hakaru.Runtime.NormTest where
+module Main where
+
+import           Language.Hakaru.Runtime.Prelude
+import qualified System.Random.MWC               as MWC
+import           Control.Monad
+
+prog :: MWC.GenIO -> IO Double
+prog = normal (real_ 0) (prob_ 3)
+
+main :: IO ()
+main = do
+  g <- MWC.createSystemRandom
+  forever $ run g prog
+  
