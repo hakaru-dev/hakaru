e2e5379a947ddd2ef5507421f9b3ac9bd474184e
Rob Zinkov <rob@zinkov.com>
2016-02-19 17:11:17 -0500

    Make tests pass without Maple

--- haskell/Tests/TestSuite.hs
+++ haskell/Tests/TestSuite.hs
@@ -1,6 +1,7 @@
 -- module Tests.TestSuite(main) where
 
 import System.Exit (exitFailure)
+import System.Environment (lookupEnv)
 
 import qualified Tests.Parser       as P
 import qualified Tests.TypeCheck    as TC
@@ -12,16 +13,25 @@ import Test.HUnit
 
 -- master test suite
 
-allTests :: Test
-allTests = test
+ignored = putStrLn "Warning: maple tests will be ignored"
+
+simplifyTests :: Maybe String -> Test
+simplifyTests env =
+  case env of
+    Just _  -> S.allTests
+    Nothing -> test ignored
+
+allTests :: Maybe String -> Test
+allTests env = test
   [ TestLabel "Parser"       P.allTests
   , TestLabel "TypeCheck"    TC.allTests
-  , TestLabel "Simplify"     S.allTests
+  , TestLabel "Simplify"     (simplifyTests env)
   , TestLabel "Disintegrate" D.allTests
   , TestLabel "Evaluate"     E.allTests
   ]
 
 main :: IO ()
 main = do
-    Counts _ _ e f <- runTestTT allTests
+    env <- lookupEnv "LOCAL_MAPLE"
+    Counts _ _ e f <- runTestTT (allTests env)
     if (e>0) || (f>0) then exitFailure else return ()
