2610f500bc6806eeebf0566509fead6c568d9e57
Rob Zinkov <rob@zinkov.com>
2016-02-18 19:29:58 -0500

    Update test to not use Observe

--- haskell/Tests/Disintegrate.hs
+++ haskell/Tests/Disintegrate.hs
@@ -61,7 +61,8 @@ normB =
 normC :: TrivialABT Term '[] ('HReal ':-> 'HMeasure 'HReal)
 normC = lam $ \y ->
         normal (real_ 0) (prob_ 1) >>= \x ->
-        ann_ (SMeasure SReal) (O.observe (normal x (prob_ 1)) y x)
+        ann_ (SMeasure SReal)
+             (withWeight (densityNormal x (prob_ 1) y) (dirac x))
 {-
 -- Eliminating some redexes of 'normC', that is:
     lam $ \y ->
