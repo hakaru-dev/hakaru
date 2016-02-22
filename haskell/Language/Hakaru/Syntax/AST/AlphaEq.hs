2a6b533daeaf068281062869413fe2bbf547e62f
wren gayle romano <wren@community.haskell.org>
2016-02-21 20:24:02 -0500

    deleted Language.Hakaru.Normalize

--- haskell/Language/Hakaru/Syntax/AST/AlphaEq.hs
+++ haskell/Language/Hakaru/Syntax/AST/AlphaEq.hs
@@ -4,7 +4,6 @@
            , PolyKinds
            , FlexibleContexts
            , ScopedTypeVariables
-           , UndecidableInstances
            #-}
 
 module Language.Hakaru.Syntax.AST.AlphaEq where
@@ -13,10 +12,26 @@ import Language.Hakaru.Syntax.ABT
 import Language.Hakaru.Syntax.AST
 import Language.Hakaru.Pretty.Concrete
 
-import Language.Hakaru.Normalize
+----------------------------------------------------------------
+----------------------------------------------------------------
+renameABT
+    :: forall abt xs a
+    .  (ABT Term abt)
+    => abt xs a
+    -> abt xs a
+renameABT ast = cataABT var bind_ syn ast
+    where
+    bind_ :: forall b x xs . Variable x -> abt xs b -> abt (x ': xs) b
+    bind_ x e =
+        let x' = x { varID = nextBind e}      
+        in bind x' (rename x x' e)
+
 
 alphaEq :: (ABT Term abt) => abt '[] a -> abt '[] a -> Bool
 alphaEq a b =
     ast2str a == ast2str b
     where
-    ast2str = show . pretty . renameABT . stripTypeAnnotations 
+    ast2str = show . pretty . renameABT 
+
+----------------------------------------------------------------
+----------------------------------------------------------- fin.
--- haskell/Tests/Simplify.hs
+++ haskell/Tests/Simplify.hs
@@ -17,8 +17,6 @@ import Language.Hakaru.Syntax.Prelude
 import Language.Hakaru.Syntax.AST.Eq()
 import Language.Hakaru.Simplify
 
-import Language.Hakaru.Normalize
-
 import Test.HUnit
 import Tests.TestTools
 
@@ -31,44 +29,55 @@ freevar :: TrivialABT Term '[] ('HMeasure 'HNat)
 freevar = v
 
 normal01T :: TrivialABT Term '[] ('HMeasure 'HReal)
-normal01T = syn (MeasureOp_ Normal
-                 :$ syn (CoerceTo_ (CCons (Continuous HContinuous_Real) CNil) :$
-                           (syn (Literal_ (LInt (-2))) :* End))
-                 :* syn (CoerceTo_ (CCons (Continuous HContinuous_Prob) CNil) :$
-                           (syn (Literal_ (LNat 1)) :* End))
-                 :* End)
+normal01T =
+    syn (MeasureOp_ Normal
+        :$ syn (CoerceTo_ (CCons (Continuous HContinuous_Real) CNil) :$
+            (syn (Literal_ (LInt (-2))) :* End))
+        :* syn (CoerceTo_ (CCons (Continuous HContinuous_Prob) CNil) :$
+            (syn (Literal_ (LNat 1)) :* End))
+        :* End)
 
 realpair :: TrivialABT Term '[] ('HMeasure (HPair 'HReal 'HReal))
-realpair = ann_ (SMeasure $ sPair SReal SReal)
-                (dirac (pair (nat2real $ nat_ 1) (nat2real $ nat_ 2)))
+realpair =
+    ann_ (SMeasure $ sPair SReal SReal)
+        (dirac (pair (nat2real $ nat_ 1) (nat2real $ nat_ 2)))
 
 unifprob, unifprob' :: TrivialABT Term '[] ('HMeasure 'HProb)
-unifprob = uniform (real_ 1) (real_ 2) >>= \x -> dirac (unsafeProb x)
+unifprob =
+    uniform (real_ 1) (real_ 2) >>= \x ->
+    dirac (unsafeProb x)
 
-unifprob' = uniform (nat2real (nat_ 1)) (nat2real (nat_ 2)) >>= \x->
-            dirac (unsafeProb x)
+unifprob' =
+    uniform (nat2real (nat_ 1)) (nat2real (nat_ 2)) >>= \x->
+    dirac (unsafeProb x)
 
-testS :: (ABT Term abt)
-      => String
-      -> abt '[] ('HMeasure a)
-      -> Assertion
+testS
+    :: (ABT Term abt)
+    => String
+    -> abt '[] ('HMeasure a)
+    -> Assertion
 testS p x = do
     _ <- simplify x `catch` handleException (p ++ ": simplify failed")
     return ()
 
-testSimplify :: ( ABT Term abt
-                , Show (abt '[] ('HMeasure a))
-                , Eq   (abt '[] ('HMeasure a)))
-               => String -> abt '[] ('HMeasure a) -> abt '[] ('HMeasure a) -> Assertion
+testSimplify
+    ::  ( ABT Term abt
+        , Show (abt '[] ('HMeasure a))
+        , Eq   (abt '[] ('HMeasure a))
+        )
+    => String
+    -> abt '[] ('HMeasure a)
+    -> abt '[] ('HMeasure a)
+    -> Assertion
 testSimplify nm x y = do
-  x' <- simplify x
-  assertEqual nm (stripTypeAnnotations y) (stripTypeAnnotations x') 
+    x' <- simplify x
+    assertEqual nm y x'
 
 allTests :: Test
 allTests = test
-   [ testSimplify "freevar" freevar freevar
-   , testSimplify "normal01T" normal01T normal01T
-   , testSimplify "realpair" realpair realpair
-   , testSimplify "unifprob" unifprob unifprob'
-   , testS "true" (triv $ ann_ (SMeasure sBool) (dirac true))
-   ]
+    [ testSimplify "freevar" freevar freevar
+    , testSimplify "normal01T" normal01T normal01T
+    , testSimplify "realpair" realpair realpair
+    , testSimplify "unifprob" unifprob unifprob'
+    , testS "true" (triv $ ann_ (SMeasure sBool) (dirac true))
+    ]
