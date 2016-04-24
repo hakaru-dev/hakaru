ccb40df741363307e4cac53b43c16d0f09943c42
zachsully <zachsully@gmail.com>
2016-04-24 09:38:39 -0400

    added my nix build file

---
+++ default.nix
@@ -0,0 +1,27 @@
+{ mkDerivation, ansi-terminal, base, Cabal, containers, directory
+, filepath, ghc-prim, HUnit, indentation, integration, logfloat
+, math-functions, mtl, mwc-random, parsec, pretty, primitive
+, process, QuickCheck, semigroups, stdenv, text, transformers
+, vector
+}:
+mkDerivation {
+  pname = "hakaru";
+  version = "0.3.0";
+  src = ./.;
+  isLibrary = true;
+  isExecutable = true;
+  buildDepends = [
+    ansi-terminal base Cabal containers directory filepath ghc-prim
+    HUnit indentation integration logfloat math-functions mtl
+    mwc-random parsec pretty primitive process semigroups text
+    transformers vector
+  ];
+  testDepends = [
+    ansi-terminal base Cabal containers ghc-prim HUnit indentation
+    integration logfloat math-functions mtl mwc-random parsec pretty
+    primitive process QuickCheck semigroups text transformers vector
+  ];
+  homepage = "http://indiana.edu/~ppaml/";
+  description = "A probabilistic programming embedded DSL";
+  license = stdenv.lib.licenses.bsd3;
+}
