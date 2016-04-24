54bd0a1ef6ee5161d916b3424c1a678948928c91
zachsully <zachsully@gmail.com>
2016-04-24 10:05:58 -0400

    nix expr update to work with nix-shell

--- default.nix
+++ default.nix
@@ -1,27 +1,31 @@
-{ mkDerivation, ansi-terminal, base, Cabal, containers, directory
-, filepath, ghc-prim, HUnit, indentation, integration, logfloat
-, math-functions, mtl, mwc-random, parsec, pretty, primitive
-, process, QuickCheck, semigroups, stdenv, text, transformers
-, vector
-}:
-mkDerivation {
-  pname = "hakaru";
-  version = "0.3.0";
-  src = ./.;
-  isLibrary = true;
-  isExecutable = true;
-  buildDepends = [
-    ansi-terminal base Cabal containers directory filepath ghc-prim
-    HUnit indentation integration logfloat math-functions mtl
-    mwc-random parsec pretty primitive process semigroups text
-    transformers vector
-  ];
-  testDepends = [
-    ansi-terminal base Cabal containers ghc-prim HUnit indentation
-    integration logfloat math-functions mtl mwc-random parsec pretty
-    primitive process QuickCheck semigroups text transformers vector
-  ];
-  homepage = "http://indiana.edu/~ppaml/";
-  description = "A probabilistic programming embedded DSL";
-  license = stdenv.lib.licenses.bsd3;
-}
+with (import <nixpkgs> {}).pkgs;
+let pkg = haskellPackages.callPackage
+            ({ mkDerivation, ansi-terminal, base, Cabal, containers, directory
+             , filepath, ghc-prim, HUnit, indentation, integration, logfloat
+             , math-functions, mtl, mwc-random, parsec, pretty, primitive
+             , process, QuickCheck, semigroups, stdenv, text, transformers
+             , vector
+             }:
+             mkDerivation {
+               pname = "hakaru";
+               version = "0.3.0";
+               src = ./.;
+               isLibrary = true;
+               isExecutable = true;
+               buildDepends = [
+                 ansi-terminal base Cabal containers directory filepath ghc-prim
+                 HUnit indentation integration logfloat math-functions mtl
+                 mwc-random parsec pretty primitive process semigroups text
+                 transformers vector cabal-install
+               ];
+               testDepends = [
+                 ansi-terminal base Cabal containers ghc-prim HUnit indentation
+                 integration logfloat math-functions mtl mwc-random parsec pretty
+                 primitive process QuickCheck semigroups text transformers vector
+               ];
+               homepage = "http://indiana.edu/~ppaml/";
+               description = "A probabilistic programming embedded DSL";
+               license = stdenv.lib.licenses.bsd3;
+             }) {};
+in
+  pkg.env
