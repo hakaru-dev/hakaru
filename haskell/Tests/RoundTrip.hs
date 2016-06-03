ec48abc2180de079d3c97dce44e394a345a685de
Rob Zinkov <rob@zinkov.com>
2016-06-03 01:25:54 -0400

    Make t65 pass

--- haskell/Tests/RoundTrip.hs
+++ haskell/Tests/RoundTrip.hs
@@ -709,12 +709,14 @@ t65 =
             (dirac unit)
             (reject sing)))
         (reject sing)
-t65' = lam $ \t ->
-       if_ (t < (fromProb (exp (real_ (-1))))) (reject sing)
-     $ if_ (t < one) (weight (unsafeProb (log (unsafeProb t) + one)))
-     $ if_ (t < one + (fromProb (exp (real_ (-1))))) (dirac unit)
-     $ if_ (t < (fromRational 2)) (weight (unsafeProb (negate (log (unsafeProb (t + real_ (-1)))))))
-     $ reject sing
+t65' =
+     lam $ \t ->
+     uniform_0_1  >>= \x->
+     withWeight (if_ (real_ 0 < (log (unsafeProb (t + x * real_ (-1))) * real_ (-1)) &&
+                      x < (t * fromProb (exp (real_ 1)) + real_ (-1)) * fromProb (exp (real_ (-1))) &&
+                      x < t)
+                 (unsafeProb (recip (t * real_ (-1) + x)))
+                 (prob_ 0)) $ (dirac unit)
 
 t66 :: (ABT Term abt) => abt '[] ('HMeasure 'HProb)
 t66 = dirac (sqrt ((prob_ 3) + sqrt (prob_ 3)))
