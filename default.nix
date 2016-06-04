874bfded861b0ad83040408187573a6b45e5d830
Zach Sullivan <zsulliva@indiana.edu>
2016-06-03 20:01:11 -0400

    made hkc dir

--- default.nix
+++ default.nix
@@ -1,31 +1,49 @@
-with (import <nixpkgs> {}).pkgs;
-let pkg = haskellPackages.callPackage
-            ({ mkDerivation, ansi-terminal, base, Cabal, containers, directory
-             , filepath, ghc-prim, HUnit, indentation, integration, logfloat
-             , math-functions, mtl, mwc-random, parsec, pretty, primitive
-             , process, QuickCheck, semigroups, stdenv, text, transformers
-             , vector, raw-strings-qq
-             }:
-             mkDerivation {
-               pname = "hakaru";
-               version = "0.3.0";
-               src = ./.;
-               isLibrary = true;
-               isExecutable = true;
-               buildDepends = [
-                 ansi-terminal base Cabal containers directory filepath ghc-prim
-                 HUnit indentation integration logfloat math-functions mtl
-                 mwc-random parsec pretty primitive process semigroups text
-                 transformers vector cabal-install raw-strings-qq
-               ];
-               testDepends = [
-                 ansi-terminal base Cabal containers ghc-prim HUnit indentation
-                 integration logfloat math-functions mtl mwc-random parsec pretty
-                 primitive process QuickCheck semigroups text transformers vector
-               ];
-               homepage = "http://indiana.edu/~ppaml/";
-               description = "A probabilistic programming embedded DSL";
-               license = stdenv.lib.licenses.bsd3;
-             }) {};
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "default" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, ansi-terminal, base, Cabal, containers
+      , directory, filepath, ghc-prim, HUnit, indentation-parsec
+      , integration, logfloat, math-functions, mtl, mwc-random
+      , optparse-applicative, parsec, pretty, primitive, process
+      , QuickCheck, raw-strings-qq, semigroups, stdenv, text
+      , transformers, vector
+      }:
+      mkDerivation {
+        pname = "hakaru";
+        version = "0.3.0";
+        src = ./.;
+        isLibrary = true;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          ansi-terminal base Cabal containers directory filepath ghc-prim
+          HUnit indentation-parsec integration logfloat math-functions mtl
+          mwc-random parsec pretty primitive process semigroups text
+          transformers vector
+        ];
+        executableHaskellDepends = [
+          base filepath mtl mwc-random optparse-applicative pretty
+          raw-strings-qq text
+        ];
+        testHaskellDepends = [
+          ansi-terminal base Cabal containers ghc-prim HUnit
+          indentation-parsec integration logfloat math-functions mtl
+          mwc-random parsec pretty primitive process QuickCheck semigroups
+          text transformers vector
+        ];
+        homepage = "http://indiana.edu/~ppaml/";
+        description = "A probabilistic programming embedded DSL";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
 in
-  pkg.env
+
+  if pkgs.lib.inNixShell then drv.env else drv
