b338abbf8cb2c80638fe889b7f823476f1f1f6b8
Chung-chieh Shan <ccshan@indiana.edu>
2016-10-27 20:27:12 -0400

    Add "maple" command for converting Hakaru to Maple

---
+++ commands/Maple.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings, DataKinds, GADTs #-}
+
+module Main where
+
+import qualified Language.Hakaru.Pretty.Maple as Maple
+import           Language.Hakaru.Syntax.AST.Transforms
+import           Language.Hakaru.Syntax.TypeCheck
+import           Language.Hakaru.Command
+
+import           Data.Text
+import qualified Data.Text.IO as IO
+import           System.IO (stderr)
+
+import           System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+      [prog] -> IO.readFile prog >>= runPretty
+      []     -> IO.getContents   >>= runPretty
+      _      -> IO.hPutStrLn stderr "Usage: pretty <file>"
+
+runPretty :: Text -> IO ()
+runPretty prog =
+    case parseAndInfer prog of
+    Left  err              -> IO.hPutStrLn stderr err
+    Right (TypedAST typ ast) -> do
+      putStrLn $ Maple.pretty (expandTransformations ast)
+      putStrLn $ ","
+      putStrLn $ Maple.mapleType typ ""
+
