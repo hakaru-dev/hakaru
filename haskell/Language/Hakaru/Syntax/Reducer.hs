b8192839af3c6dbccb74bfd922ac46458661f37f
Rob Zinkov <rob@zinkov.com>
2017-01-31 16:53:25 -0500

    Clarify kind signatures and add to cabal file

--- haskell/Language/Hakaru/Syntax/Reducer.hs
+++ haskell/Language/Hakaru/Syntax/Reducer.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE DataKinds
            , GADTs
            , KindSignatures
+           , Rank2Types
            #-}
 
 module Language.Hakaru.Syntax.Reducer where
@@ -8,15 +9,17 @@ module Language.Hakaru.Syntax.Reducer where
 import Language.Hakaru.Types.DataKind
 import Language.Hakaru.Types.HClasses
 
-data Reducer :: (Hakaru -> *) -> Hakaru -> * where
+data Reducer (abt :: [Hakaru] -> Hakaru -> *)
+             (xs  :: [Hakaru])
+             (a :: Hakaru) where
      Red_Add
          :: HSemiring a
          -> abt xs a
-         -> Reducer (abt xs) a
+         -> Reducer abt xs a
      Red_Index
          :: abt xs 'HNat               -- size of resulting array
-         -> abt ( 'HNat ': xs ) 'HNat -- index into array (bound i)
-         -> Reducer (abt ( 'HNat ': xs)) a
-         -> Reducer (abt xs) a
+         -> abt ( 'HNat ': xs ) 'HNat  -- index into array (bound i)
+         -> Reducer abt ( 'HNat ': xs) a
+         -> Reducer abt xs a
 
 
