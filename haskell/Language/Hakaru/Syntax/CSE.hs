943dc91232749ffbe1462ad2a39201069cfe68d6
Spenser Bauman <sabauma@gmail.com>
2017-02-01 15:58:34 -0500

    Some initial work on CSE pass

--- haskell/Language/Hakaru/Syntax/CSE.hs
+++ haskell/Language/Hakaru/Syntax/CSE.hs
@@ -13,6 +13,8 @@
 {-# LANGUAGE TypeOperators              #-}
 module Language.Hakaru.Syntax.CSE where
 
+import           Prelude hiding ((+))
+import           Control.Monad.Reader
 import qualified Data.IntMap                     as IM
 import           Data.Maybe
 import           Data.Number.Nat
@@ -29,6 +31,12 @@ import           Language.Hakaru.Types.DataKind
 
 import           Language.Hakaru.Syntax.Prelude hiding (fst, (.))
 
+example1 :: TrivialABT Term '[] 'HReal
+example1 = triv $ real_ 1 + real_ 2 + real_ 3
+
+example2 :: TrivialABT Term '[] 'HReal
+example2 = triv $ real_ 6
+
 -- What we need is an environment like data structure which maps Terms (or
 -- general abts?) to other abts. Can such a mapping be implemented efficiently?
 -- This would seem to require a hash operation to make efficient.
@@ -39,6 +47,11 @@ data EAssoc (abt :: [Hakaru] -> Hakaru -> *)
 -- An association list for now
 newtype Env (abt :: [Hakaru] -> Hakaru -> *) = Env [EAssoc abt]
 
+emptyEnv :: Env a
+emptyEnv = Env []
+
+-- NB: This code could potentially produce an infinite loop depending on how
+-- terms are added to the environment. How do we want to prevent this?
 lookupEnv
   :: forall abt a . (ABT Term abt)
   => abt '[] a
@@ -50,8 +63,9 @@ lookupEnv ast (Env env) = go ast env
     go ast []                = ast
     go ast (EAssoc a b : xs) =
       case jmEq1 (typeOf ast) (typeOf a) of
-        Nothing   -> go ast xs
-        Just Refl -> if alphaEq ast a then go b env else go ast xs
+        Nothing                   -> go ast xs
+        Just Refl | alphaEq ast a -> go b env
+                  | otherwise     -> go ast xs
 
 insertEnv
   :: forall abt a
@@ -61,6 +75,28 @@ insertEnv
   -> Env abt
 insertEnv ast1 ast2 (Env env) = Env (EAssoc ast1 ast2 : env)
 
+newtype CSE (abt :: [Hakaru] -> Hakaru -> *) a = CSE { runCSE :: Reader (Env abt) a }
+  deriving (Functor, Applicative, Monad, MonadReader (Env abt))
+
 cse :: (ABT Term abt) => abt '[] a -> abt '[] a
-cse = undefined
+cse abt = runReader (runCSE (cse' abt)) emptyEnv
+
+cse' :: (ABT Term abt) => abt '[] a -> CSE abt (abt '[] a)
+cse' abt = caseVarSyn abt cseVar cseTerm
+
+-- Variables can be equivalent to other variables
+-- TODO: A good sanity check would be to ensure the result in this case is
+-- always a variable or constant. A variable should never be substituted for
+-- a more complex expression.
+cseVar
+  :: (ABT Term abt)
+  => Variable a
+  -> CSE abt (abt '[] a)
+cseVar v = lookupEnv (var v) `fmap` ask
+
+cseTerm
+  :: (ABT Term abt)
+  => Term abt a
+  -> CSE abt (abt '[] a)
+cseTerm = undefined
 
