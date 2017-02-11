db917c7a03f2317da50877ee55ca8aebe866d03b
Spenser Bauman <sabauma@gmail.com>
2017-02-11 00:58:20 -0500

    Add support for constant bindings to the constant propagation pass

--- haskell/Language/Hakaru/Evaluation/ConstantPropagation.hs
+++ haskell/Language/Hakaru/Evaluation/ConstantPropagation.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE CPP
-           , GADTs
-           , DataKinds
-           , Rank2Types
-           , ScopedTypeVariables
-           , MultiParamTypeClasses
-           , FlexibleContexts
-           , FlexibleInstances
-           #-}
+{-# LANGUAGE CPP                        #-}
+{-# LANGUAGE DataKinds                  #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GADTs                      #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE Rank2Types                 #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
 
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
 ----------------------------------------------------------------
@@ -26,14 +26,21 @@ module Language.Hakaru.Evaluation.ConstantPropagation
     ) where
 
 #if __GLASGOW_HASKELL__ < 710
-import           Data.Functor         ((<$>))
-import           Control.Applicative  (Applicative(..))
+import           Control.Applicative                  (Applicative (..))
+import           Data.Functor                         ((<$>))
 #endif
 
-import Language.Hakaru.Syntax.IClasses (Traversable21(..))
-import Language.Hakaru.Syntax.ABT      (View(..), ABT(..), cataABT)
-import Language.Hakaru.Syntax.AST
-import Language.Hakaru.Evaluation.EvalMonad (runPureEvaluate)
+import           Control.Monad.Reader
+import           Language.Hakaru.Evaluation.EvalMonad (runPureEvaluate)
+import           Language.Hakaru.Syntax.ABT           (ABT (..), View (..))
+import           Language.Hakaru.Syntax.AST
+import           Language.Hakaru.Syntax.IClasses      (Traversable21 (..))
+import           Language.Hakaru.Syntax.Variable
+
+type Env = Assocs Literal
+
+newtype PropM a = PropM { runPropM :: Reader Env a }
+  deriving (Functor, Applicative, Monad, MonadReader Env)
 
 ----------------------------------------------------------------
 ----------------------------------------------------------------
@@ -48,21 +55,56 @@ import Language.Hakaru.Evaluation.EvalMonad (runPureEvaluate)
 --
 -- | Perform basic constant propagation.
 constantPropagation
-    :: forall abt a. (ABT Term abt) => abt '[] a -> abt '[] a
-constantPropagation =
-    cataABT var bind alg
-    where
-    getLiteral :: forall xs b. abt xs b -> Maybe (abt xs b)
-    getLiteral e =
-        case viewABT e of
-        Syn (Literal_ _) -> Just e
-        _                -> Nothing
+  :: forall abt a . (ABT Term abt)
+  => abt '[] a
+  -> abt '[] a
+constantPropagation abt = runReader (runPropM $ constantProp' abt) emptyAssocs
 
-    alg :: forall b. Term abt b -> abt '[] b
-    alg t =
-        case traverse21 getLiteral t of
-        Nothing -> syn t
-        Just t' -> runPureEvaluate (syn t')
+constantProp'
+  :: forall abt a xs . (ABT Term abt)
+  => abt xs a
+  -> PropM (abt xs a)
+constantProp' = start
+  where
 
-----------------------------------------------------------------
------------------------------------------------------------ fin.
+    start :: forall b ys . abt ys b -> PropM (abt ys b)
+    start = loop . viewABT
+
+    loop :: forall b ys . View (Term abt) ys b -> PropM (abt ys b)
+    loop (Var v)    = (maybe (var v) (syn . Literal_) . lookupAssoc v) <$> ask
+    loop (Syn s)    = constantPropTerm s --alg <$> traverse21 start s
+    loop (Bind v b) = bind v <$> loop b
+
+
+tryEval :: forall abt b . (ABT Term abt) => Term abt b -> abt '[] b
+tryEval t =
+  case traverse21 getLit t of
+    Nothing -> syn t
+    Just t' -> runPureEvaluate (syn t')
+  where
+    getLit :: forall a ys . abt ys a -> Maybe (abt ys a)
+    getLit l = case viewABT l of
+                 Syn (Literal_ _) -> Just l
+                 _                -> Nothing
+
+getLiteral :: forall abt ys b. (ABT Term abt) => abt ys b -> Maybe (Literal b)
+getLiteral e =
+  case viewABT e of
+    Syn (Literal_ l) -> Just l
+    _                -> Nothing
+
+constantPropTerm
+  :: (ABT Term abt)
+  => Term abt a
+  -> PropM (abt '[] a)
+constantPropTerm (Let_ :$ rhs :* body :* End) =
+  caseBind body $ \v body' -> do
+    rhs' <- constantProp' rhs
+    case getLiteral rhs' of
+      Just l  -> local (insertAssoc (Assoc v l)) (constantProp' body')
+      Nothing -> do
+        body'' <- constantProp' body'
+        return $ syn (Let_ :$ rhs' :* bind v body'' :* End)
+
+constantPropTerm term = tryEval <$> traverse21 constantProp' term
+
