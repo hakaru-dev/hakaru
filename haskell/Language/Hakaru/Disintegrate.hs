9f445354dcd9c06539e48f980061d94287fe2ba3
Praveen Narayanan <pravnar@indiana.edu>
2017-02-21 19:15:05 -0500

    Shorten code and sketch out literal array indexing in Disintegrate

--- haskell/Language/Hakaru/Disintegrate.hs
+++ haskell/Language/Hakaru/Disintegrate.hs
@@ -516,7 +516,7 @@ indexArrayOp :: forall abt typs args a r
                 , typs ~ UnLCs args, args ~ LCs typs )
              => ArrayOp typs a
              -> SArgs abt args
-             -> TermEvaluator abt (Dis abt)
+             -- -> TermEvaluator abt (Dis abt)
              -> (abt '[] a -> Dis abt r) -- evaluate or (constrainValue v0)
              -> (Head abt ('HArray a)    -- e1 is in whnf, and
                 -> Variable 'HNat        -- e2 is a free under current indices
@@ -529,12 +529,19 @@ indexArrayOp :: forall abt typs args a r
                 -> Variable 'HNat        -- e2 is a free under current indices
                 -> Dis abt r)
              -> Dis abt r
-indexArrayOp o@(Index _) (e1 :* e2 :* End) evaluate_ kInd kArr kSyn kFree kMultiLoc = do
+indexArrayOp o@(Index _) (e1 :* e2 :* End) kInd kArr kSyn kFree kMultiLoc = do
   w1 <- evaluate_ e1
   case w1 of
     Head_ arr@(WArray _ b)  -> caseBind b $ \x body ->
       evalIndex (kInd . flip (rename x) body) (kArr arr)
     Head_ (WEmpty _)        -> error "TODO: indexArrayOp o (Empty_ :* _ :* End)"
+    Head_ (WArrayLiteral [a1,a2]) ->
+        case (isLitBool a1, isLitBool a2) of
+          (Just b1, Just b2)
+              | isLitTrue b1 && isLitFalse b2 -> undefined
+              | isLitTrue b2 && isLitFalse b1 -> undefined
+              | otherwise -> undefined
+          _ -> error "TODO: indexArrayOp o (ArrayLiteral_ :* [a1,a2] :* End)"
     Head_ (WArrayLiteral _) -> error "TODO: indexArrayOp o (ArrayLiteral_ :* _ :* End)"
     Head_ _          -> error "indexArrayOp: unknown whnf of array type"
     Neutral e1' -> flip (caseVarSyn e1') kSyn $ \x ->
@@ -556,7 +563,23 @@ indexArrayOp o@(Index _) (e1 :* e2 :* End) evaluate_ kInd kArr kSyn kFree kMulti
               flip (caseVarSyn term) (const bot) $ \v -> -- bot if index is neutral syntax
                 do isInd <- isIndex v
                    if isInd then thenCase v else elseCase v
-indexArrayOp _ _ _ _ _ _ _ _ = error "indexArrayOp called on incorrect ArrayOp"
+indexArrayOp _ _ _ _ _ _ _ = error "indexArrayOp called on incorrect ArrayOp"
+
+isLitBool :: (ABT Term abt) => abt '[] a -> Maybe (Datum (abt '[]) HBool)
+isLitBool e = caseVarSyn e (const Nothing) $ \b ->
+                  case b of
+                    Datum_ d@(Datum _ typ _) -> case (jmEq1 typ sBool) of
+                                                  Just Refl -> Just d
+                                                  Nothing   -> Nothing
+                    _ -> Nothing
+
+isLitTrue :: (ABT Term abt) => Datum (abt '[]) HBool -> Bool
+isLitTrue (Datum tdTrue sBool (Inl Done)) = True
+isLitTrue _                               = False
+
+isLitFalse :: (ABT Term abt) => Datum (abt '[]) HBool -> Bool
+isLitFalse (Datum tdFalse sBool (Inr (Inl Done))) = True
+isLitFalse _                                      = False
 
 evaluateArrayOp
     :: ( ABT Term abt
@@ -570,7 +593,6 @@ evaluateArrayOp evaluate_ = go
     go o@(Index _) = \args@(_ :* e2 :* End) ->
       let returnIndex = return . Neutral . syn
       in indexArrayOp o args
-                      evaluate_
                       evaluate_
                       (\arr v -> returnIndex (ArrayOp_ o :$ fromHead arr :* var v :* End))
                       (\s     -> returnIndex (ArrayOp_ o :$ syn s        :* e2    :* End))
@@ -1143,7 +1165,6 @@ constrainValue v0 e0 =
                                                    -- TODO use meta-index
         ArrayLiteral_ _          -> error "TODO: disintegrate literal arrays"
         ArrayOp_ o@(Index _) :$ args -> indexArrayOp o args
-                                                     evaluate_
                                                      (constrainValue v0)
                                                      (const $ const bot)
                                                      (const bot)
