b07f00d4e1228a7500a8b1ed697de8ca1a6d221b
Chad Scherrer <chad.scherrer@gmail.com>
2017-03-21 10:58:34 -0700

    missing imports

--- examples/tcp/app/Main.hs
+++ examples/tcp/app/Main.hs
@@ -3,7 +3,7 @@ module Main where
 import qualified Data.ByteString.Char8 as B
 import Data.News (getNews)
 import qualified System.Random.MWC as MWC
-import Scaffold (samples)
+import Scaffold (samples, z0, next)
 
 main = do
   (words, docs) <- getNews (Just 5) [0,1]
