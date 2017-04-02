a193709bb76750b40f2f68e0754b4b4be9d25b23
Rob Zinkov <rob@zinkov.com>
2017-04-02 07:26:09 -0400

    Add accuracy count

--- examples/tcp/app/Main.hs
+++ examples/tcp/app/Main.hs
@@ -13,7 +13,7 @@ import Data.List (sort)
 import Data.Number.LogFloat
 
 main = do
-  (words, docs, topics) <- getNews (Just 100) [0,1]
+  (words, docs, topics) <- getNews (Just 100) [0..]
   g <- MWC.create
   let 
     zPrior = onesFrom topics
@@ -24,10 +24,20 @@ main = do
   printf "length words  == %d\n" (V.length words)
   printf "length docs   == %d\n" (V.length docs)
   printf "length topics == %d\n" (V.length topics)
-  forM_ [0..(V.length topics - 1)] $ \i -> do
-    --print $ V.map logFromLogFloat $ predict i
-    printf "%d %d\n" (topics ! i) (V.maxIndex $ predict i)
+  -- forM_ [0..(V.length topics - 1)] $ \i -> do
+  --   --print $ V.map logFromLogFloat $ predict i
+  --   printf "%d %d\n" (topics ! i) (V.maxIndex $ predict i)
+  print $ accuracy
+            topics
+            (V.map (V.maxIndex . predict) (V.generate (V.length topics) id))
   -- replicateM 5 . withGen g (print . sort) $ do
   --   pred <- predict
   --   let p = pred $ V.fromList $ [0,1,7]
   --   replicateM 100 p
+
+accuracy
+    :: V.Vector Int
+    -> V.Vector Int
+    -> Double
+accuracy x y = V.sum z / (fromIntegral $ V.length x)
+    where z = V.zipWith (\a b -> if a == b then 1 else 0) x y
