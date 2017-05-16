2068f22f221bbd1c7e0cceea75f95202df086209
Praveen Narayanan <pravnar@indiana.edu>
2017-05-15 20:30:57 -0400

    Make the disintegrate command work on lambda terms
    We go from a term of type:
    
    a1 -> a2 -> ... -> aN -> M(a,b)
    
    to a term of type:
    
    a1 -> a2 -> ... -> aN -> a -> M(b)

--- commands/Disintegrate.hs
+++ commands/Disintegrate.hs
@@ -1,12 +1,22 @@
-{-# LANGUAGE OverloadedStrings, PatternGuards, DataKinds, GADTs #-}
+{-# LANGUAGE OverloadedStrings
+           , PatternGuards
+           , TypeOperators
+           , DataKinds
+           , GADTs
+           , KindSignatures
+           , FlexibleContexts
+           , RankNTypes #-}
 
 module Main where
 
 import           Language.Hakaru.Pretty.Concrete
 import           Language.Hakaru.Syntax.TypeCheck
 import           Language.Hakaru.Syntax.IClasses
+import           Language.Hakaru.Syntax.ABT
+import           Language.Hakaru.Syntax.AST as T
 
 import           Language.Hakaru.Types.Sing
+import           Language.Hakaru.Types.DataKind
 import           Language.Hakaru.Disintegrate
 import           Language.Hakaru.Evaluation.ConstantPropagation
 import           Language.Hakaru.Command
@@ -28,13 +38,36 @@ main = do
 runDisintegrate :: Text -> IO ()
 runDisintegrate prog =
     case parseAndInfer prog of
-    Left  err                -> IO.hPutStrLn stderr err
-    Right (TypedAST typ ast) ->
-        case typ of
-          SMeasure (SData (STyCon sym `STyApp` _ `STyApp` _) _)
-            | Just Refl <- jmEq1 sym sSymbol_Pair
-            -> case determine (disintegrate ast) of
-               Just ast' -> print . pretty $ constantPropagation ast'
-               Nothing   -> IO.hPutStrLn stderr "No disintegration found"
-          _ -> IO.hPutStrLn stderr "Can only disintegrate a measure over pairs"
-
+    Left  err      -> IO.hPutStrLn stderr err
+    Right typedAST -> go Nil1 typedAST
+    where
+      go :: List1 Variable (xs :: [Hakaru])
+         -> TypedAST (TrivialABT T.Term)
+         -> IO ()
+      go xs (TypedAST typ ast)
+          = case typ of
+              SMeasure (SData (STyCon sym `STyApp` _ `STyApp` _) _)
+                  | Just Refl <- jmEq1 sym sSymbol_Pair
+                     -> case determine (disintegrate ast) of
+                          Just ast' ->
+                              lams xs (print.pretty.constantPropagation) ast'
+                          Nothing   -> IO.hPutStrLn stderr
+                                       "No disintegration found"
+              SFun _ b ->
+                caseVarSyn ast (const putErrorMsg) $ \t ->
+                case t of
+                  Lam_ :$ body :* End ->
+                      caseBind body $ \x e ->
+                      go (append1 xs (Cons1 x Nil1)) (TypedAST b e)
+                  _ -> putErrorMsg
+              _ -> putErrorMsg
+                   
+putErrorMsg :: IO ()
+putErrorMsg = IO.hPutStrLn stderr
+              "Can only disintegrate (functions over) measures over pairs"
+              
+lams :: (ABT T.Term abt)
+     => List1 Variable (xs :: [Hakaru])
+     -> (forall a. abt '[] a -> IO ()) -> abt '[] a -> IO ()
+lams Nil1         k = k
+lams (Cons1 x xs) k = lams xs (k . lam_ x)
