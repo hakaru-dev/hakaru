872fbcdc5a4c43179b8849dc5ab52b303470fd97
GenevaS <GenevaS@users.noreply.github.com>
2017-05-23 13:00:25 -0400

    Delete install_mkdocs_Windows.cmd
    Instructions moved to "Adding a Language Feature" documentation

--- install_mkdocs_Windows.cmd
+++
@@ -1,7 +0,0 @@
-REM You must have Pip installed to use this script. Pip has been bundled with both Python 2.7.9 and Python 3.4,
-REM so it will be installed when Python is. 
-
-pip install mkdocs
-pip install python-markdown-math
-pip install mkdocs-extensions
-pip install mkdocs-bootswatch
