f0841f0d478671e3613ac43e3b8df9d301f03db9
yuriy <toporoy@mcmaster.ca>
2017-06-20 21:04:04 -0400

    Add more options to pretty command

--- commands/Pretty.hs
+++ commands/Pretty.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, DataKinds, GADTs #-}
+{-# LANGUAGE OverloadedStrings, DataKinds, GADTs, CPP, RecordWildCards #-}
 
 module Main where
 
@@ -7,16 +7,49 @@ import           Language.Hakaru.Syntax.AST.Transforms
 import           Language.Hakaru.Syntax.TypeCheck
 import           Language.Hakaru.Command
 
-import           Data.Text
+import           Data.Text (Text)
+import qualified Data.Text as T 
 import qualified Data.Text.Utf8 as IO
 import           System.IO (stderr)
+import           Data.Monoid
+
+#if __GLASGOW_HASKELL__ < 710
+import           Control.Applicative   (Applicative(..), (<$>), (<*>))
+#endif
+import qualified Options.Applicative as O
+
+data Options = Options
+  { printType :: Bool 
+  , internal  :: Bool 
+  , program   :: FilePath 
+  }
+
+options :: O.Parser Options
+options = Options
+  <$> O.switch
+      ( O.short 't' <>
+        O.long "print-type" <>
+        O.help "Print the type of the expression in internal Haskell syntax as well as the expression itself." )
+  <*> O.switch
+      ( O.short 'i' <>
+        O.long "internal-syntax" <>
+        O.help "Print the expression in internal Haskell syntax instead of in concrete syntax." )
+  <*> O.strArgument
+      ( O.metavar "PROGRAM" <> 
+        O.help "Filename containing program to be simplified, or \"-\" to read from input." ) 
+
+parseOpts :: IO Options
+parseOpts = O.execParser $ O.info (O.helper <*> options)
+      (O.fullDesc <> O.progDesc "Pretty print a Hakaru program")
 
 main :: IO ()
-main = simpleCommand runPretty "pretty"
+main = parseOpts >>= runPretty 
 
-runPretty :: Text -> IO ()
-runPretty prog =
+runPretty :: Options -> IO ()
+runPretty Options{..} = readFromFile program >>= \prog -> 
     case parseAndInfer prog of
-    Left  err              -> IO.hPutStrLn stderr err
-    Right (TypedAST _ ast) -> IO.print . pretty . expandTransformations $ ast
+    Left  err               -> IO.hPutStrLn stderr err
+    Right (TypedAST ty ast) -> IO.putStrLn $
+      T.concat [ T.pack . (if internal then show else show.pretty) . expandTransformations $ ast 
+               , if printType then "\n,\n" <> T.pack (show ty) else "" ]
 
