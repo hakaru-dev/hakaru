9f0a388c6e101213c96b6f31870d53821505b214
yuriy <toporoy@mcmaster.ca>
2017-06-27 23:57:06 -0400

    Remove the hack to parse `lebesgue' without arguments.
    And update all the programs which would now be broken.

--- haskell/Language/Hakaru/Parser/SymbolResolve.hs
+++ haskell/Language/Hakaru/Parser/SymbolResolve.hs
@@ -110,9 +110,7 @@ primTable =
     ,("nat2prob",    primCoerce cNat2Prob)
     ,("nat2int",     primCoerce cNat2Int)
      -- Measures
-    ,("lebesgue",    TNeu $ syn $ U.MeasureOp_ (U.SomeOp T.Lebesgue) 
-                       [syn $ U.PrimOp_ U.Negate [syn $ U.PrimOp_ U.Infinity []], 
-                        syn $ U.PrimOp_ U.Infinity []])
+    ,("lebesgue",    primMeasure2 (U.SomeOp T.Lebesgue))
     ,("counting",    TNeu $ syn $ U.MeasureOp_ (U.SomeOp T.Counting) [])
     ,("uniform",     primMeasure2 (U.SomeOp T.Uniform))
     ,("normal",      primMeasure2 (U.SomeOp T.Normal))
@@ -285,13 +283,6 @@ symbolResolution symbols ast =
         name' <- gensym name
         U.Lam (mkSym name') typ
             <$> symbolResolution (insertSymbol name' symbols) x
-
-    U.App (U.App (U.Var "lebesgue") x) y -> 
-      U.App <$> 
-         (U.App
-                 (U.Var $ primMeasure2 (U.SomeOp T.Lebesgue))
-             <$> symbolResolution symbols x)
-            <*> symbolResolution symbols y
 
     U.App f x -> U.App
         <$> symbolResolution symbols f
