54387b115e0e2d61778bcccfe87fc5f5015bf121
yuriy <toporoy@mcmaster.ca>
2017-07-13 04:47:17 -0400

    Try a new implementation of underLam'; the old one was clearly wrong
    The new one doesn't seem to work either (for easyroad.hk), but simple test cases
    do work.

--- haskell/Language/Hakaru/Syntax/AST/Transforms.hs
+++ haskell/Language/Hakaru/Syntax/AST/Transforms.hs
@@ -5,6 +5,7 @@
            , DataKinds
            , TypeOperators
            , ViewPatterns
+           , OverloadedStrings
            #-}
 
 {-# OPTIONS_GHC -Wall -fwarn-tabs #-}
@@ -22,16 +23,20 @@ import Language.Hakaru.Syntax.ABT
 import Language.Hakaru.Syntax.AST
 import Language.Hakaru.Syntax.TypeOf
 import Language.Hakaru.Syntax.IClasses
+import Language.Hakaru.Syntax.Prelude (lamWithVar, app)
 import Language.Hakaru.Types.DataKind
 
 import Language.Hakaru.Expect       (expect)
 import Language.Hakaru.Disintegrate (determine, observe)
 
 import Data.Ratio (numerator, denominator)
-import Language.Hakaru.Types.Sing (sing, Sing(..))
+import Language.Hakaru.Types.Sing (sing, Sing(..), sUnFun)
 import Language.Hakaru.Types.HClasses (HFractional(..))
 import Language.Hakaru.Types.Coercion (findCoercion, Coerce(..))
 import qualified Data.Sequence as Seq 
+import Control.Monad.Fix (MonadFix)
+
+import Debug.Trace
 
 
 optimizations
@@ -73,25 +78,52 @@ underLam f e = caseVarSyn e (return . var) $ \t ->
                    _ -> error "TODO: underLam"
 
 underLam'
-    :: (ABT Term abt, Monad m)
+    :: forall abt m a b b'
+     . (ABT Term abt, MonadFix m)
     => (abt '[] b -> m (abt '[] b'))
     -> abt '[] (a ':-> b)
     -> m (abt '[] (a ':-> b'))
-underLam' f e = caseVarSyn e (error "underLam': unbound var (?)") $ \t ->
-                   case t of
-                   Lam_ :$ e1 :* End ->
-                       caseBind e1 $ \x e1' -> do
-                           e1'' <- f e1'
-                           return . syn $
-                                  Lam_  :$ (bind x e1'' :* End)
+underLam' f e = do
+  f' <- trace "underLam': build function" $
+        fmap (\f' b -> app (syn $ Lam_ :$ f' :* End) b) $
+        binderM "" (snd $ sUnFun $ typeOf e) f
+  return $ underLam'p f' e
 
-                   Let_ :$ e1 :* e2 :* End ->
-                       caseBind e2 $ \x e2' -> do
-                         e2'' <- underLam' f e2'
-                         return . syn $
-                                Let_ :$ e1 :* (bind x e2'') :* End
+underLam'p
+    :: forall abt a b b'
+     . (ABT Term abt)
+    => (abt '[] b -> abt '[] b')
+    -> abt '[] (a ':-> b)
+    -> abt '[] (a ':-> b')
+underLam'p f e =
+  let var_ :: Variable (a ':-> b) -> abt '[] (a ':-> b')
+      var_ v_ab = trace "underLam': entered var" $
+        lamWithVar "" (fst $ sUnFun $ varType v_ab) $ \a ->
+        trace "underLam': applied function" $ f $ app (var v_ab) a
 
-                   _ -> error "TODO: underLam'"
+      syn_ t = trace "underLam': entered syn" $
+        case t of
+        Lam_ :$ e1 :* End -> trace "underLam': entered syn/Lam_" $
+          caseBind e1 $ \x e1' ->
+            trace "underLam': rebuilt Lam_" $
+            syn $ Lam_  :$
+                (trace "underLam': applied bind{Lam_}" $
+                      bind x (trace "underLam': applied function{Lam_}"
+                                $ f e1')) :* End
+
+        Let_ :$ e1 :* e2 :* End -> trace "underLam': entered syn/Lam_" $
+          caseBind e2 $ \x e2' ->
+            trace "underLam': rebuilt Let_" $
+            syn $ Let_ :$ e1 :*
+                  (trace "underLam': applied bind{Lam_}" $
+                         bind x (trace "underLam': recursive case{Let_}" $
+                                       go e2')) :* End
+
+        _ -> error "TODO: underLam'"
+
+      go e' = trace "underLam': entered main body" $
+              caseVarSyn e' var_ syn_
+  in go e
 
 expandTransformations
     :: forall abt a
