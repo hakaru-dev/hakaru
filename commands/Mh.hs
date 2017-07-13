fcc7b5bea470ed7e442e645c56e171b183c3b704
yuriy <toporoy@mcmaster.ca>
2017-07-13 02:32:23 -0400

    Implement front-end mcmc with back-end command

--- commands/Mh.hs
+++ commands/Mh.hs
@@ -18,6 +18,8 @@ import           Language.Hakaru.Syntax.AST (Term(..))
 import           Language.Hakaru.Types.Sing
 import           Language.Hakaru.Types.DataKind (Hakaru(..))
 import           Language.Hakaru.Inference
+import           Language.Hakaru.Syntax.Command (dynCommand'Pure)
+import           Language.Hakaru.Syntax.Prelude (pair_)
 import           Language.Hakaru.Command hiding (Term)
   
 import           Data.Text
@@ -39,7 +41,8 @@ runMH :: Text -> Text -> IO ()
 runMH prog1 prog2 =
     case (parseAndInfer prog1, parseAndInfer prog2) of
       (Right (TypedAST typ1 ast1), Right (TypedAST typ2 ast2)) ->
-         either (IO.hPutStrLn stderr) (print . pretty) $
+         either (IO.hPutStrLn stderr)
+                (elimTypedAST $ \_ -> print . pretty) $
          runMH' typ1 ast1 typ2 ast2
       (Left err, _) -> IO.hPutStrLn stderr err
       (_, Left err) -> IO.hPutStrLn stderr err
@@ -47,35 +50,8 @@ runMH prog1 prog2 =
 runMH' :: (ABT Term abt)
        => Sing a -> abt '[] a
        -> Sing b -> abt '[] b
-       -> Either Text (abt '[] a)
+       -> Either Text (TypedAST abt)
 runMH' propty prop tgtty tgt =
-  case (propty, tgtty) of
-    -- TODO: figure out how to write this case
-    -- (_, SFun a tgtty')                -> _
-    (SFun a (SMeasure b), SMeasure c) ->
-      case (jmEq1 a b, jmEq1 b c) of
-        (Just Refl, Just Refl) -> Right $ mcmc prop tgt
-        (Nothing  , Just _   ) ->
-          typeMismatchErr "proposal" (Left "x -> measure x") (Right propty)
-        (Just _   , Nothing  ) ->
-          typeMismatchErr "target" (Right $ SMeasure b) (Right tgtty)
-
-    (_                  , SMeasure _) ->
-      typeMismatchErr "proposal" (Left "x -> measure x") (Right propty)
-
-    (SFun a (SMeasure b), _         ) ->
-      typeMismatchErr "target" (Left "measure x") (Right tgtty)
-
-    _                                 ->
-      typeMismatchErr "input" (Left "(measure x, x -> measure x)")
-                              (Right $ sPair tgtty propty)
-
-typeMismatchErr
-  :: Text
-  -> Either Text (Sing (a :: Hakaru))
-  -> Either Text (Sing (b :: Hakaru))
-  -> Either Text x
-typeMismatchErr pref expt got = Left $
-  mconcat[ "mh: ", pref, " has wrong type\n"
-         , "expected ", either id prettyTypeT expt , "\n"
-         , "but got ", either id prettyTypeT got ]
+  either (Left . pack . show) Right $
+  dynCommand'Pure dynMCMC $
+  TypedAST (sPair propty tgtty) $ pair_ propty tgtty prop tgt
