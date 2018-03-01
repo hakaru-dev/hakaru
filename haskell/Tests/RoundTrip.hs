e9e38c4545edd4097757b43307229ff8f4e8106c
sohraa3 <arian_sohrabi@hotmail.com>
2018-03-01 00:58:54 -0500

    Raleigh to chiSq test

--- haskell/Tests/RoundTrip.hs
+++ haskell/Tests/RoundTrip.hs
@@ -209,6 +209,10 @@ testStdChiSqRelations = test [
     "t_stdChiSq_to_gamma"   ~: testConcreteFiles "tests/RoundTrip/t_stdChiSq_to_gamma.0.hk" "tests/RoundTrip/t_stdChiSq_to_gamma.expected.hk",
     "t_stdChiSq_to_exponential" ~: testConcreteFiles "tests/RoundTrip2/t_stdChiSq_to_exponential.0.hk" "tests/RoundTrip2/t_stdChiSq_to_exponential.expected.hk"
         ]
+        
+testChiSq :: Test
+testChiSq = test [
+    "test Chi Dist"     ~: testConcreteFiles "tests/RoundTrip2/t_Rayleigh_to_ChiSquared.0.hk" "tests/RoundTrip2/t_Rayleigh_to_ChiSquared.expected.hk"
 
 testOther :: Test
 testOther = test [
