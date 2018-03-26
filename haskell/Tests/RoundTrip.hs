bcedb2193aff98e0f0e5c617c35215115cfe5bd3
sohraa3 <arian_sohrabi@hotmail.com>
2018-03-26 15:51:42 -0400

    3 tests written

--- haskell/Tests/RoundTrip.hs
+++ haskell/Tests/RoundTrip.hs
@@ -203,6 +203,13 @@ testMeasurePair = test [
     "easyHMM"           ~: testConcreteFile "tests/RoundTrip/easyHMM.hk",
     "testMCMCPriorProp" ~: testConcreteFile "tests/RoundTrip/testMCMCPriorProp.hk"
     ]
+testErlangRelations :: Test
+testErlangRelations = test [
+    "t_erlang_to_erlang"   ~: testConcreteFiles "tests/RoundTrip2/t_erlang_to_erlang.0.hk" "tests/RoundTrip2/t_erlang_to_erlang.expected.hk",
+    "t_erlang_to_erlang_2"   ~: testConcreteFiles "tests/RoundTrip2/t_erlang_to_erlang_1.0.hk" "tests/RoundTrip2/t_erlang_to_erlang_1.expected.hk",
+    "t_exponential_to_erlang"   ~: testConcreteFiles "tests/RoundTrip2/t_exponential_to_erlang.0.hk" "tests/RoundTrip2/t_exponential_to_erlang.expected.hk"
+
+    ]
 
 testOther :: Test
 testOther = test [
@@ -245,6 +252,7 @@ allTests = test
     , testMeasurePair
     , testMeasureNat
     , testMeasureInt
+    , testErlangRelations
     , testOther
     ]
 
