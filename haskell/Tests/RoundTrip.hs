36d4e53d0fa2f53e91b4da383a3759e7e545d193
Justin Staples <staplejw@mcmaster.ca>
2018-04-09 13:09:03 -0400

    adding test for transformation from uniform to cauchy

--- haskell/Tests/RoundTrip.hs
+++ haskell/Tests/RoundTrip.hs
@@ -211,6 +211,11 @@ testStdChiSqRelations = test [
     "t_rayleigh_to_stdChiSq"     ~: testConcreteFiles "tests/RoundTrip2/t_rayleigh_to_stdChiSq.0.hk" "tests/RoundTrip2/t_rayleigh_to_stdChiSq.expected.hk"        
     ]
 
+testCauchyRelations :: Test 
+testCauchyRelations = test [
+    "t_uniform_to_cauchy" ~: testConcreteFiles "tests/RoundTrip2/t_uniform_to_cauchy.0.hk" "tests/RoundTrip2/t_uniform_to_cauchy.expected.hk"
+    ]
+
 testOther :: Test
 testOther = test [
     "t82"              ~: testConcreteFiles "tests/RoundTrip/t82.0.hk" "tests/RoundTrip/t82.expected.hk",
