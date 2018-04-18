0347319561609ce4bfe7f95116828e94b4439062
Mahmoud Khattab <khattm@mcmaster.ca>
2018-04-17 21:23:16 -0400

    Exponential to Beta

--- haskell/Tests/RoundTrip.hs
+++ haskell/Tests/RoundTrip.hs
@@ -213,6 +213,7 @@ testStdChiSqRelations = test [
 
 testExponentialRelations :: Test 
 testExponentialRelations = test [ 
+    "t_exponential_to_beta"        ~: testConcreteFiles "tests/RoundTrip/t_exponential_to_beta.0.hk" "tests/RoundTrip/t_exponential_to_beta.expected.hk",
     "t_exponential_to_stdChiSq"     ~: testConcreteFiles "tests/RoundTrip/t_exponential_to_stdChiSq.0.hk" "tests/RoundTrip/t_exponential_to_stdChiSq.expected.hk"
     ]
 
