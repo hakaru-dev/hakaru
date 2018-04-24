1dec053be03feca9c6c5b199b747a0e9c743ce41
Mahmoud Khattab <khattm@mcmaster.ca>
2018-04-24 04:24:43 -0400

    Fix RoundTrip

--- haskell/Tests/RoundTrip.hs
+++ haskell/Tests/RoundTrip.hs
@@ -247,9 +247,9 @@ testErlangRelations = test [
 
 testRayleighRelations :: Test 
 testRayleighRelations = test [
-    "t_exponential_to_rayleigh" ~: testConcreteFiles "tests/RoundTrip2/t_exponential_to_rayleigh.0.hk" "tests/RoundTrip2/t_exponential_to_rayleigh.expected.hk",
-    "t_rayleigh_to_gamma" ~: testConcreteFiles "tests/RoundTrip2/t_rayleigh_to_gamma.0.hk" "tests/RoundTrip2/t_rayleigh_to_gamma.expected.hk",
-    "t_weibull_to_rayleigh" ~: testConcreteFiles "tests/RoundTrip2/t_weibull_to_rayleigh.0.hk" "tests/RoundTrip2/t_weibull_to_rayleigh.expected.hk"
+    "t_exponential_to_rayleigh" ~: testConcreteFiles "tests/RoundTrip/t_exponential_to_rayleigh.0.hk" "tests/RoundTrip/t_exponential_to_rayleigh.expected.hk",
+    "t_rayleigh_to_gamma" ~: testConcreteFiles "tests/RoundTrip/t_rayleigh_to_gamma.0.hk" "tests/RoundTrip/t_rayleigh_to_gamma.expected.hk",
+    "t_weibull_to_rayleigh" ~: testConcreteFiles "tests/RoundTrip/t_weibull_to_rayleigh.0.hk" "tests/RoundTrip/t_weibull_to_rayleigh.expected.hk"
     ]
 
 testOther :: Test
